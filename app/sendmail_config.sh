5e1bd19d8014baf29ac1c826d2fa9b1f3c4cefd3
lauxley <tissotrobin@gmail.com>
2019-02-25 22:08:59 +0100

    sendmail install

---
+++ app/sendmail_config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# set host in hosts
+line=$(head -n 1 /etc/hosts)
+line2=$(echo $line | awk '{print $2}')
+echo "$line $line2.localdomain" >> /etc/hosts
+
+apt-get install -y sendmail sendmail-cf m4 \
+    && hostname >> /etc/mail/relay-domains \
+    && m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf
+
+#remove localhost limit
+sed -i -e "s/Port=smtp,Addr=127.0.0.1, Name=MTA/Port=smtp, Name=MTA/g" \
+    /etc/mail/sendmail.mc
+sendmail -bd
