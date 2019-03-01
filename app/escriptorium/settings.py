9836ec183c823100641c66e53fc76cf69a4b6f60
lauxley <tissotrobin@gmail.com>
2019-03-01 19:22:39 +0100

    Fixes old staticfiles?

--- app/escriptorium/settings.py
+++ app/escriptorium/settings.py
@@ -164,6 +164,9 @@ CHANNEL_LAYERS = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
