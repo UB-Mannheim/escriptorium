8305201e9916ade6fbd6b6b50cada731db8b8f7c
Robin <tissotrobin@gmail.com>
2019-03-19 13:37:51 +0100

    Forgoten file

---
+++ app/apps/core/templatetags/bleach.py
@@ -0,0 +1,11 @@
+import bleach
+
+from django import template
+
+
+register = template.Library()
+
+@register.filter
+def strip_html(obj, tags=None):
+    tags = tags or None
+    return bleach.clean(obj, strip=True, tags=tags)
