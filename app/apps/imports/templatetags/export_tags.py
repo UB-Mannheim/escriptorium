201393ccb1b5d5b12b88b438427f452c31b57168
Hassan Gargem <hgargem@LEX-01550.local>
2019-11-27 16:26:38 +0100

    box coordinate from models to templatetags

--- app/apps/imports/templatetags/export_tags.py
+++ app/apps/imports/templatetags/export_tags.py
@@ -8,9 +8,15 @@ register = template.Library()
 def current_time():
     return timezone.now().isoformat()
 
+
 @register.simple_tag
 def line_break(lines):
     return "{} \n".format(lines)
+
+# coordinates in pagexml template <TextRegion coords ="x1,x2..xn,yn>
+@register.simple_tag
+def box_coordinates(box):
+    return ' '.join(','.join(map(str, pt)) for pt in box)
 
 
 
