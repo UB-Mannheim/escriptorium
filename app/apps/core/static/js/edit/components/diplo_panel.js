5d735dfa3dc0faab41b8b45cc1c6cdab64879fac
elhassane <elhassanegargem@gmail.com>
2020-03-23 18:12:22 +0100

    togglesave button

--- app/apps/core/static/js/edit/components/diplo_panel.js
+++ app/apps/core/static/js/edit/components/diplo_panel.js
@@ -1,5 +1,32 @@
 var DiploPanel = BasePanel.extend({
+
+    data() { return {
+        editLine: null,
+        save: false, //show save button
+    };},
     components: {
         'diploline': diploLine,
     },
+    methods:{
+        togglePanel(){},
+        saveContent(){
+            console.log("call saveContent");
+        },
+        toggleSave(){
+            let timer = 0;
+            clearTimeout(timer);
+            timer = setTimeout(function (){
+            if(this.save){
+               this.saveContent();
+            }
+            this.save = !this.save;
+        }.bind(this), 1000);
+        },
+        editNext() {
+            ev.preventDefault();
+            // TODO
+            console.log("call editableContent",this.$parent);
+        },
+    },
+
 });
