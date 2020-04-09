c9af6441865c9946d63de88f2a6580d30e91e1b0
elhassane <elhassanegargem@gmail.com>
2020-04-09 12:10:32 +0200

    fix content encoding

--- app/apps/core/static/js/edit/components/diplo_line.js
+++ app/apps/core/static/js/edit/components/diplo_line.js
@@ -19,14 +19,15 @@ var diploLine = LineBase.extend({
 
             clearTimeout(timer);
             timer = setTimeout(function (){
-                this.setContent(this.$refs.content[0].innerHTML);
-                if(this.line.transcription.pk){
-                    this.$parent.$emit('update:transcription:content', this.line.transcription);
 
-                }
-                else {
+                //set content of input to line content
+                this.line.transcription.content = this.$refs.content[0].textContent;
+                if(this.line.transcription.pk)
+                    this.$parent.$emit('update:transcription:content', this.line.transcription);
+                else
                  this.$parent.$emit('create:transcription', this.line.transcription);
-                }
+
+                // call save of parent method
                  this.$parent.toggleSave();
             }.bind(this), 1000);
 
@@ -38,6 +39,29 @@ var diploLine = LineBase.extend({
                 this.line.transcription.content = content;
                 moment.tz(this.line.transcription.version_updated_at, this.timeZone);
 
+        },
+        onPast(e){
+          let pastedData = e.clipboardData.getData('text/plain');
+          let pasted_data_split = pastedData.split('\n');
+          console.log("on past",pasted_data_split);
+
+          if(pasted_data_split.length < 2){
+                return
+          }
+          else {
+            e.preventDefault();
+            document.execCommand('inserttext', false, pasted_data_split[0]);
+
+            if(pasted_data_split[pasted_data_split.length -1] =="")
+                pasted_data_split.pop();
+            let index = $(this).parent().index();
+            let lines = $(".line-content");
+            for (let i=index+1; i<index + pasted_data_split.length; i++){
+                let value = pasted_data_split[i- index];
+                $(lines[i]).text(value);
+                // self.editAndSave(lines[i]);
+                }
+            }
         }
     }
 });
