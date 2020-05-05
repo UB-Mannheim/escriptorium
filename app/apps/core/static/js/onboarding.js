5f1c94c32e55ed37596cf8288cb7a289fe5ecb7a
elhassane <elhassanegargem@gmail.com>
2020-05-05 12:54:47 +0200

    onboarding content

--- app/apps/core/static/js/onboarding.js
+++ app/apps/core/static/js/onboarding.js
@@ -4,56 +4,56 @@ document_images_intro.setOptions({
 steps: [
 {
   element: '#nav-doc-tab',
-  intro: 'This guided tour will explain the Hongkiat demo page interface.<br><br>Use the arrow keys for navigation or hit ESC to exit the tour immediately.',
+  intro: 'Update Document description (Name, Text direction or metadata).<br>',
   position: 'bottom'
 },
 {
   element: '#nav-edit-tab',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Edit Document Part. Panels to update transcriptions, baselines and masks',
   position: 'bottom'
 },
     {
   element: '#nav-models-tab',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Handle Transcription and Segmentation models related to this document.',
   position: 'bottom'
 },  {
   element: '#document-image-dropzone',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Upload a new document part image.',
   position: 'bottom'
 },
   {
   element: '#import-selected',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Import document part. <br> accepted formats : IIIF, Pagexml, Alto.',
   position: 'bottom'
 },
   {
   element: '#document-export',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Import document part. <br> accepted formats : Text, Pagexml, Alto.',
   position: 'bottom'
 },
 {
   element: '#train-selected',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Train a Segmentation or Transcription model',
   position: 'bottom'
 },
 {
   element: '#binarize-selected',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Binarize the color of selected images.',
   position: 'bottom'
 },
 {
   element: '#segment-selected',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Segment selected images.',
   position: 'bottom'
 },
 {
   element: '#transcribe-selected',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Transcribe automatically the selected images.',
   position: 'top'
 },
 {
   element: '#js-edit',
-  intro: 'Click this main logo to view a list of all Hongkiat demos.',
+  intro: 'Transcribe, Segment Manually the image.',
   position: 'bottom'
 },
 ]});
