b52a0517bc07bd3d1078f52cd56e45abd014318a
Robin Tissot <tissotrobin@gmail.com>
2020-05-15 08:49:33 +0200

    Fixes default admin email to point to my own.

--- app/escriptorium/settings.py
+++ app/escriptorium/settings.py
@@ -18,7 +18,8 @@ from django.utils.translation import gettext_lazy as _
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-ADMINS = [('Robin Tissot','tissotrobin@gmail.com'),]
+ADMINS = [(os.getenv('DJANGO_ADMIN_NAME', 'admin'),
+           os.getenv('DJANGO_ADMIN_EMAIL', 'admin@example.com'))]
 
 # Add apps directory the sys.path
 APPS_DIR = os.path.join(BASE_DIR, 'apps')
