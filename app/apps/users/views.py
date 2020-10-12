a0988d46d032f9ccd053585523dbe7e187b96d0f
elhassane <elhassanegargem@gmail.com>
2020-10-12 16:20:19 +0200

    onboarding for update not create

--- app/apps/users/views.py
+++ app/apps/users/views.py
@@ -102,6 +102,8 @@ class Profile(SuccessMessageMixin, UpdateView):
         page_number = self.request.GET.get('page')
         context['is_paginated'] = paginator.count != 0
         context['page_obj'] = paginator.get_page(page_number)
+        context['onboarding'] = self.request.user.onboarding
+
         return context
 
 class ContactUsView(SuccessMessageMixin, CreateView):
