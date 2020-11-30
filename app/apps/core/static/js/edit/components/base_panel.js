82dfba35ead14a3ee555102f125a5801d9278646
Robin Tissot <tissotrobin@gmail.com>
2020-11-30 17:59:35 +0100

    Fixes text not loading in text panel, line not being scrolled into view when moving caret and text panel height.

--- app/apps/core/static/js/edit/components/base_panel.js
+++ app/apps/core/static/js/edit/components/base_panel.js
@@ -5,11 +5,6 @@ const BasePanel = Vue.extend({
             ratio: 1
         };
     },
-    watch: {
-        'ratio': function(n, o) {
-            if (this.part.loaded) this.refresh();
-        }
-    },
     methods: {
         setRatio() {
             this.ratio = this.$el.firstChild.clientWidth / this.part.image.size[0];
