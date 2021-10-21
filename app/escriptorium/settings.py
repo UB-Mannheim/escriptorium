1ac72322f64886efe5439db2b0d7f236bb64f731
Stefan Weil <sw@weilnetz.de>
2021-10-21 15:17:40 +0200

    Add local settings for UB Mannheim installation
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/settings.py
+++ app/escriptorium/settings.py
@@ -296,12 +296,12 @@ LOGGING = {
     },
     'handlers': {
         'file': {
-            'level': 'ERROR',
+            'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(PROJECT_ROOT, 'logs', 'error.log'),
         },
         'console': {
-            'level': 'ERROR',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         },
         'console_info': {
@@ -314,7 +314,7 @@ LOGGING = {
             'class': 'logging.StreamHandler',
         },
         'django.server': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
             'formatter': 'django.server',
         },
