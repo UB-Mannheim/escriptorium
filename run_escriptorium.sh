89155396ca7324bf3e9fb63b8e14ccd5d82e9e46
Stefan Weil <sw@weilnetz.de>
2021-10-21 15:17:40 +0200

    Add local settings for UB Mannheim installation
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-source /home/stweil/src/gitlab/scripta/escriptorium/venv/bin/activate
+source /home/stweil/src/gitlab/scripta/escriptorium/venv3.9/bin/activate
 export DJANGO_SETTINGS_MODULE=escriptorium.local_settings
 export ESC_LANGUAGES="de,en,fr"
 
-cd /home/stweil/src/gitlab/scripta/escriptorium/app
-celery worker --app escriptorium --concurrency 2 --loglevel DEBUG &
-python -Wa manage.py runserver --settings escriptorium.local_settings --verbosity 3 0.0.0.0:8080
+celery worker --app escriptorium --concurrency 2 --loglevel INFO &
+python manage.py runserver --settings escriptorium.local_settings --verbosity 2 0.0.0.0:8080
