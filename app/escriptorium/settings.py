4aec1fa9ef38371e23bb9b0c2f984d4dd4998e7b
Stefan Weil <sw@weilnetz.de>
2021-11-05 09:25:32 +0100

    Activate localization
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/settings.py
+++ app/escriptorium/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
