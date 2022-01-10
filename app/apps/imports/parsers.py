UB-Mannheim/escriptorium

commit 2110ece2c5a3b0bdc5c4c0800d5b6fd246594a93

Don't interupt an import when some coords are empty. Solves #470.

app/apps/imports/parsers.py
@@ -340,7 +340,7 @@ class XMLParser(ParserDocument):
                                 except ValidationError as e:
                                     if self.report:
                                         self.report.append(
-                                            "Block in '{filen}' line N°{line} was skipped because: {error}".format(
+                                            _("Block in '{filen}' line N°{line} was skipped because: {error}").format(
                                                 filen=self.file.name, line=blockTag.sourceline, error=e))
                                 else:
                                     block.save()
@@ -365,9 +365,12 @@ class XMLParser(ParserDocument):
                                 line.full_clean()
                             except ValidationError as e:
                                 if self.report:
-                                    self.report.append("Line in '{filen}' line N°{line} (id: {lineid}) was skipped because: {error}".format(
-                                        filen=self.file.name, line=blockTag.sourceline,
-                                        lineid=line_id, error=e))
+                                    self.report.append(
+                                        _("Line in '{filen}' line N°{line} (id: {lineid}) was skipped because: {error}")
+                                        .format(filen=self.file.name,
+                                                line=blockTag.sourceline,
+                                                lineid=line_id,
+                                                error=e))
                             else:
                                 line.save()
 
@@ -467,8 +470,8 @@ The ALTO file should contain a Description/sourceImageInformation/fileName tag f
                     coords = tuple(map(float, baseline.split(" ")))
                     line.baseline = tuple(zip(coords[::2], coords[1::2]))
                 except ValueError:
-                    msg = ("Invalid baseline %s in {filen} line {linen}" %
-                           (baseline, self.file.name, lineTag.sourceline))
+                    msg = _("Invalid baseline %s in {filen} line {linen}").format(
+                        baseline, self.file.name, lineTag.sourceline)
                     logger.warning(msg)
                     if self.report:
                         self.report.append(msg)
@@ -581,13 +584,16 @@ The PAGE file should contain an attribute imageFilename in Page tag for matching
         try:
             baseline = lineTag.find("Baseline", self.root.nsmap)
             line.baseline = self.clean_coords(baseline)
-        except AttributeError:
+        except ParseError:
             #  to check if the baseline is good
             line.baseline = None
 
-        polygon = lineTag.find("Coords", self.root.nsmap)
-        if polygon is not None:
-            line.mask = self.clean_coords(polygon)
+        try:
+            polygon = lineTag.find("Coords", self.root.nsmap)
+            if polygon is not None:
+                line.mask = self.clean_coords(polygon)
+        except ParseError:
+            line.mask = None
 
         type_ = lineTag.get("type")
         if not type_:
@@ -606,10 +612,16 @@ The PAGE file should contain an attribute imageFilename in Page tag for matching
                 pass
 
     def clean_coords(self, coordTag):
-        return [
+        try:
+            return [
                 list(map(int, pt.split(",")))
                 for pt in coordTag.get("points").split(" ")
             ]
+        except (AttributeError, ValueError):
+            msg = _("Invalid coordinates for {tag} in {filen} line {line}").format(
+                tag=coordTag.tag, filen=self.file.name, line=coordTag.sourceline)
+            self.report.append(msg)
+            raise ParseError(msg)
 
     def get_transcription_content(self, lineTag):
         words = lineTag.findall("Word", self.root.nsmap)
@@ -661,7 +673,7 @@ class IIIFManifestParser(ParserDocument):
         be transient.  It will only retry a fixed number of times (default 10),
         and it backs off a little more on each retry. Failure to retrieve
         the image within the retry limit will result in a DownloadError
-        being raised. All other unsuccessful requests will raise a 
+        being raised. All other unsuccessful requests will raise a
         DownloadError as well.
         """
 
@@ -675,10 +687,10 @@ class IIIFManifestParser(ParserDocument):
 
             except requests.exceptions.HTTPError as http_error:
                 # retry on transient 5XX errors, but keep a record of the retry count
-                if http_error.response.status_code in [500, 502, 503, 504, 507, 508]: 
+                if http_error.response.status_code in [500, 502, 503, 504, 507, 508]:
                     current_retry = current_retry + 1
                     continue
-                
+
                 # We probably got a 4XX error, but whatever it is just raise it
                 raise DownloadError(http_error)
 

app/apps/core/models.py
@@ -1153,6 +1153,11 @@ class Line(OrderedModel):  # Versioned,
     def make_external_id(self):
         self.external_id = 'eSc_line_%s' % str(uuid.uuid4())[:8]
 
+    def clean(self):
+        super().clean()
+        if self.baseline is None and self.mask is None:
+            raise ValidationError(_('Both baseline and mask are empty.'))
+
     def save(self, *args, **kwargs):
         if self.external_id is None:
             self.make_external_id()

app/apps/imports/tasks.py
@@ -67,11 +67,13 @@ def document_import(task, import_pk, resume=True, task_id=None, user_pk=None, re
         imp.report.error(str(e))
     else:
         if user:
-            user.notify(_("Import done!"), level='success')
-
-        send_event('document', imp.document.pk, "import:done", {
-            "id": imp.document.pk
-        })
+            if imp.report.messages:
+                user.notify(_("Import finished with warnings!"),
+                            links=[{'text': _('Details'), 'src': imp.report.uri}],
+                            level='warning')
+            else:
+                user.notify(_("Import done!"), level='success')
+        send_event('document', imp.document.pk, "import:done", {"id": imp.document.pk})
         imp.report.end()
 
 

commit c46b87770ae1dbc6dfc39cdbf2313652ea5b9262

Fix error message in class AltoParser function update_line
Fixes: 2110ece2c ("Don't interupt an import when some coords are empty")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

app/apps/imports/parsers.py
@@ -480,8 +480,8 @@ The ALTO file should contain a Description/sourceImageInformation/fileName tag f
                     coords = tuple(map(float, baseline.split(" ")))
                     line.baseline = tuple(zip(coords[::2], coords[1::2]))
                 except ValueError:
-                    msg = _("Invalid baseline %s in {filen} line {linen}").format(
-                        baseline, self.file.name, lineTag.sourceline)
+                    msg = _("Invalid baseline {baseline} in {filen} line {linen}").format(
+                        baseline=baseline, filen=self.file.name, linen=lineTag.sourceline)
                     logger.warning(msg)
                     if self.report:
                         self.report.append(msg)