f9d0e403d9bdce5d7593b35d8c5f35936e7c01dd
Stefan Weil <sw@weilnetz.de>
2022-02-12 12:39:22 +0100

    Test urlpatterns for media (w.i.p.)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/urls.py
+++ app/escriptorium/urls.py
@@ -45,5 +45,5 @@ if settings.DEBUG:
                     path('404/', TemplateView.as_view(template_name='404.html'))]
 
     # Serve static files in development
+    urlpatterns += static('media/', document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
