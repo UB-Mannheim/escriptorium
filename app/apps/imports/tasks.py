99ad06f56174214bf334b0dc2d2eed750f35dcbb
Eva Bardou <ebardou@teklia.com>
2022-02-14 15:06:01 +0000

    Refactor existing document_export code into classes

--- app/apps/imports/tasks.py
+++ app/apps/imports/tasks.py
@@ -1,20 +1,16 @@
-from datetime import datetime
 import logging
 import os.path
-from zipfile import ZipFile
 
 from django.apps import apps
 from django.conf import settings
-from django.db.models import Q, Prefetch, Avg
-from django.template import loader
-from django.utils.text import slugify
+from django.db.models import Q
 from django.utils.translation import gettext as _
 
 from celery import shared_task
 
-from core.models import Line
 from users.consumers import send_event
 from escriptorium.utils import send_email
+from imports.export import EXPORTER_CLASS
 from reporting.tasks import create_task_reporting
 
 
@@ -82,15 +78,9 @@ def document_import(task, import_pk=None, resume=True, task_id=None, user_pk=Non
 def document_export(task, file_format, part_pks,
                     transcription_pk, region_types, document_pk=None, include_images=False,
                     user_pk=None, report_label=None):
-    ALTO_FORMAT = "alto"
-    PAGEXML_FORMAT = "pagexml"
-    TEXT_FORMAT = "text"
-
     User = apps.get_model('users', 'User')
     Document = apps.get_model('core', 'Document')
-    DocumentPart = apps.get_model('core', 'DocumentPart')
     Transcription = apps.get_model('core', 'Transcription')
-    LineTranscription = apps.get_model('core', 'LineTranscription')
     TaskReport = apps.get_model('reporting', 'TaskReport')
 
     user = User.objects.get(pk=user_pk)
@@ -107,96 +97,17 @@ def document_export(task, file_format, part_pks,
             "id": document.pk
         })
 
-        # Check if we have to include orphan lines
-        include_orphans = False
-        if 'Orphan' in region_types:
-            include_orphans = True
-            region_types.remove('Orphan')
-
-        # Check if we have to include lines with an undefined region type
-        include_undefined = False
-        if 'Undefined' in region_types:
-            include_undefined = True
-            region_types.remove('Undefined')
+        if file_format not in EXPORTER_CLASS:
+            raise NotImplementedError(f"File format {file_format} isn't a supported format during a data export")
 
         transcription = Transcription.objects.get(document=document, pk=transcription_pk)
-
-        base_filename = "export_doc%d_%s_%s_%s" % (
-            document.pk,
-            slugify(document.name).replace('-', '_')[:32],
-            file_format,
-            datetime.now().strftime('%Y%m%d%H%M'))
-
-        if file_format == TEXT_FORMAT:
-            filename = "%s.txt" % base_filename
-            filepath = os.path.join(user.get_document_store_path(), filename)
-            # content_type = 'text/plain'
-
-            region_filters = Q(line__block__typology_id__in=region_types)
-            if include_orphans:
-                region_filters |= Q(line__block__isnull=True)
-            if include_undefined:
-                region_filters |= Q(line__block__isnull=False, line__block__typology_id__isnull=True)
-
-            lines = (LineTranscription.objects
-                     .filter(transcription=transcription, line__document_part__pk__in=part_pks)
-                     .filter(region_filters)
-                     .exclude(content="")
-                     .order_by('line__document_part', 'line__document_part__order', 'line__order'))
-            # return StreamingHttpResponse(['%s\n' % line.content for line in lines],
-            #                              content_type=content_type)
-            with open(filepath, 'w') as fh:
-                fh.writelines(['%s\n' % line.content for line in lines])
-                fh.close()
-
-        elif file_format == ALTO_FORMAT or file_format == PAGEXML_FORMAT:
-            filename = "%s.zip" % base_filename
-            filepath = os.path.join(user.get_document_store_path(), filename)
-            # buff = io.BytesIO()
-            if file_format == ALTO_FORMAT:
-                tplt = loader.get_template('export/alto.xml')
-            elif file_format == PAGEXML_FORMAT:
-                tplt = loader.get_template('export/pagexml.xml')
-            parts = DocumentPart.objects.filter(document=document, pk__in=part_pks)
-
-            region_filters = Q(typology_id__in=region_types)
-            if include_undefined:
-                region_filters |= Q(typology_id__isnull=True)
-
-            with ZipFile(filepath, 'w') as zip_:
-                for part in parts:
-                    render_orphans = {} if not include_orphans else {
-                        'orphan_lines': part.lines.prefetch_transcription(transcription).filter(block=None)
-                    }
-
-                    if include_images:
-                        # Note adds image before the xml file
-                        zip_.write(part.image.path, part.filename)
-                    try:
-                        page = tplt.render({
-                            'valid_block_types': document.valid_block_types.all(),
-                            'valid_line_types': document.valid_line_types.all(),
-                            'part': part,
-                            'blocks': (part.blocks.filter(region_filters)
-                                       .annotate(avglo=Avg('lines__order'))
-                                       .order_by('avglo')
-                                       .prefetch_related(
-                                           Prefetch(
-                                               'lines',
-                                               queryset=Line.objects.prefetch_transcription(
-                                                   transcription)))),
-                            **render_orphans
-                        })
-                    except Exception as e:
-                        report.append("Skipped {element}({image}) because '{reason}'.".format(
-                            element=part.name, image=part.filename, reason=str(e)
-                        ))
-                    else:
-                        zip_.writestr('%s.xml' % os.path.splitext(part.filename)[0], page)
-
-                zip_.close()
-
+        exporter = EXPORTER_CLASS[file_format](
+            part_pks, region_types, include_images, user, document, report, transcription
+        )
+        exporter.render()
     except Exception as e:
+        report.error(str(e))
+
         if user:
             user.notify(_("Something went wrong during the export!"),
                         links=[{'text': 'Report', 'src': report.uri}],
@@ -207,13 +118,12 @@ def document_export(task, file_format, part_pks,
             "id": document.pk,
             "reason": str(e)
         })
-        logger.exception(e)
-        report.error(str(e))
 
+        logger.exception(e)
     else:
-        rel_path = os.path.relpath(filepath, settings.MEDIA_ROOT)
         report.end()
 
+        rel_path = os.path.relpath(exporter.filepath, settings.MEDIA_ROOT)
         user.notify(_('Export done!'),
                     level='success',
                     links=[{'text': _('Download'),
