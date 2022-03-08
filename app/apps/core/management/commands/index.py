377ca949dadb2903400564aa43ceefb71ff02f1c
Robin Tissot <tissotrobin@gmail.com>
2022-03-08 16:03:01 +0100

    isort - fix pipeline.

--- app/apps/core/management/commands/index.py
+++ app/apps/core/management/commands/index.py
@@ -1,12 +1,12 @@
 import logging
 
-from core.models import Project
+from django.conf import settings
+from django.core.management.base import BaseCommand
 from elasticsearch import Elasticsearch
 from elasticsearch.client import IndicesClient
 from elasticsearch.helpers import bulk as es_bulk
 
-from django.core.management.base import BaseCommand
-from django.conf import settings
+from core.models import Project
 
 logger = logging.getLogger('es_indexing')
 
