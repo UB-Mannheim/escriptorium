d9b6fbf5ec1aa8d483d6f8c6ee36e6feacbc806f
Stefan Weil <sw@weilnetz.de>
2022-03-24 18:16:42 +0100

    Use escriptorium.local_settings by default
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/celery.py
+++ app/escriptorium/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escriptorium.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escriptorium.local_settings')
 
 app = Celery('escriptorium')
 
