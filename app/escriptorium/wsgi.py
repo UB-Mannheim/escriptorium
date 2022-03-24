55e2b054556228b34a3931acd94fa02595844e9e
Stefan Weil <sw@weilnetz.de>
2022-03-24 18:16:42 +0100

    Use escriptorium.local_settings by default
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/wsgi.py
+++ app/escriptorium/wsgi.py
@@ -11,6 +11,18 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escriptorium.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'escriptorium.local_settings')
 
 application = get_wsgi_application()
+
+"""
+def application(environ, start_response):
+    status = '200 OK'
+    output = b'Hello World!'
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
+"""
