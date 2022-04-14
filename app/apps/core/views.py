7f54c00d22de8c06bfe3c86b381e20ad7ff38a69
Eva Bardou <ebardou@teklia.com>
2022-04-14 18:24:53 +0000

    Index and display more information about search results

--- app/apps/core/views.py
+++ app/apps/core/views.py
@@ -159,7 +159,11 @@ class Search(LoginRequiredMixin, FormView, TemplateView):
         ])
         return {
             'content': hit.get('highlight', {}).get('content', [])[0],
+            'line_number': hit_source['line_number'],
+            'transcription_name': hit_source['transcription_name'],
+            'part_title': hit_source['part_title'],
             'part_pk': hit_source['document_part_id'],
+            'document_name': hit_source['document_name'],
             'document_pk': hit_source['document_id'],
             'score': hit['_score'],
             'img_url': hit_source['image_url'],
