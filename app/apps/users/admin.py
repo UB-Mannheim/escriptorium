fa15a04ccb289307a4aaf813b4adb7b6ccdc7c21
Robin Tissot <tissotrobin@gmail.com>
2022-05-24 16:08:58 +0200

    Adds date_joined to the table fields in users admin.

--- app/apps/users/admin.py
+++ app/apps/users/admin.py
@@ -38,7 +38,7 @@ class MyUserCreationForm(UserCreationForm):
 class MyUserAdmin(UserAdmin):
     form = MyUserChangeForm
     add_form = MyUserCreationForm
-    list_display = UserAdmin.list_display + ('is_active', 'last_login', 'quota_disk_storage', 'quota_cpu', 'quota_gpu')
+    list_display = UserAdmin.list_display + ('is_active', 'last_login', 'date_joined', 'quota_disk_storage', 'quota_cpu', 'quota_gpu')
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('fields', 'onboarding')}),  # second fields refers to research fields
         ('Quotas management (if not defined, fallback to instance quotas)', {'fields': ('quota_disk_storage', 'quota_cpu', 'quota_gpu')}),
