3c45309119e503e1c61d4dcee0f79566e5626970
Ben Silverman <ben@performantsoftware.com>
2022-07-07 11:03:43 -0400

    Resolve migration conflict

--- app/apps/core/migrations/0063_linetranscription_avg_confidence.py
+++ app/apps/core/migrations/0063_linetranscription_avg_confidence.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0061_auto_20220419_1414"),
+        ("core", "0062_auto_20220608_1737"),
     ]
 
     operations = [
