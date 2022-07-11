0a86ea998738ab37cfdcd29801ae05ea214887ab
Stefan Weil <sw@weilnetz.de>
2022-07-11 13:31:38 +0200

    Add start script for systemd
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

---
+++ run_escriptorium.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /home/stweil/src/gitlab/scripta/escriptorium/venv/bin/activate
+export DJANGO_SETTINGS_MODULE=escriptorium.local_settings
+export ESC_LANGUAGES="de,en,fr"
+
+celery worker --app escriptorium --concurrency 2 --loglevel DEBUG &
+python -Wa manage.py runserver --settings escriptorium.local_settings --verbosity 3 0.0.0.0:8080
