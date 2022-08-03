UB-Mannheim/escriptorium

commit c2769aa9a5b9eb586211303ec8a56776e89f7577

Fixes empty regions not being imported, simplify the export templates, fixes #209.

app/apps/imports/templatetags/export_tags.py
@@ -15,16 +15,6 @@ def line_break(lines):
 
 
 @register.simple_tag
-def box_coordinates(block, part):
-    # coordinates in pagexml template <TextRegion coords ="x1,x2..xn,yn>
-    if block is not None:
-        return ' '.join(','.join(map(lambda x: str(int(x)), pt)) for pt in block.box)
-    else:
-        w, h = part.image.width, part.image.height
-        return '0,0 %d,0 %d,%d 0,%d' % (w, w, h, h)
-
-
-@register.simple_tag
 def pagexml_points(points):
     return ' '.join(','.join(map(lambda x: str(int(x)), pt)) for pt in points)
 

app/apps/core/models.py
@@ -10,7 +10,7 @@ from PIL import Image
 from datetime import datetime
 
 from django.db import models, transaction
-from django.db.models import Q
+from django.db.models import Q, Prefetch
 from django.db.models.signals import pre_delete
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -825,6 +825,16 @@ class Block(OrderedModel, models.Model):
         return super().save(*args, **kwargs)
 
 
+class LineManager(models.Manager):
+    def prefetch_transcription(self, transcription):
+        return (self.get_queryset().order_by('order')
+                                   .prefetch_related(
+                                       Prefetch('transcriptions',
+                                                to_attr='transcription',
+                                                queryset=LineTranscription.objects.filter(
+                                                    transcription=transcription))))
+
+
 class Line(OrderedModel):  # Versioned,
     """
     Represents a segmented line from a DocumentPart
@@ -837,7 +847,7 @@ class Line(OrderedModel):  # Versioned,
     document_part = models.ForeignKey(DocumentPart,
                                       on_delete=models.CASCADE,
                                       related_name='lines')
-    block = models.ForeignKey(Block, null=True, blank=True, on_delete=models.SET_NULL)
+    block = models.ForeignKey(Block, null=True, blank=True, on_delete=models.SET_NULL, related_name='lines')
     script = models.CharField(max_length=8, null=True, blank=True)  # choices ??
     # text direction
     order_with_respect_to = 'document_part'
@@ -847,6 +857,8 @@ class Line(OrderedModel):  # Versioned,
                                  on_delete=models.SET_NULL)
 
     external_id = models.CharField(max_length=128, blank=True, null=True)
+
+    objects = LineManager()
 
     class Meta(OrderedModel.Meta):
         pass

app/apps/imports/tasks.py
@@ -11,6 +11,7 @@ from django.utils.text import slugify
 
 from celery import shared_task
 
+from core.models import Line
 from users.consumers import send_event
 from escriptorium.utils import send_email
 
@@ -106,12 +107,15 @@ def document_export(task, file_format, user_pk, document_pk, part_pks, transcrip
                     'valid_block_types': document.valid_block_types.all(),
                     'valid_line_types': document.valid_line_types.all(),
                     'part': part,
-                    'lines': part.lines.order_by('block__order', 'order')
-                                       .prefetch_related(
-                                           Prefetch('transcriptions',
-                                                    to_attr='transcription',
-                                                    queryset=LineTranscription.objects.filter(
-                                                        transcription=transcription)))})
+                    'blocks': (part.blocks.order_by('order')
+                                          .prefetch_related(
+                                             Prefetch(
+                                                 'lines',
+                                                 queryset=Line.objects.prefetch_transcription(transcription)))),
+
+                    'orphan_lines': (part.lines.prefetch_transcription(transcription)
+                                               .filter(block=None))
+                })
                 zip_.writestr('%s.xml' % os.path.splitext(part.filename)[0], page)
         zip_.close()
 

app/escriptorium/templates/export/alto.xml
@@ -23,33 +23,28 @@
                   VPOS="0"
                   WIDTH="{{ part.image.width }}"
                   HEIGHT="{{ part.image.height }}">
-        {% for line in lines %}{% ifchanged line.block %}{% if not forloop.first %}</TextBlock>{% endif %}
-        <TextBlock {% if line.block %}HPOS="{{ line.block.coordinates_box.0 }}"
-                   VPOS="{{ line.block.coordinates_box.1 }}"
-                   WIDTH="{{ line.block.width }}"
-                   HEIGHT="{{ line.block.height }}"
-                   ID="{{ line.block.external_id }}"
-                   {% if line.block.typology %}TAGREFS="BT{{line.block.typology.id}}"{% endif %}>
-          <Shape><Polygon POINTS="{% alto_points line.block.box %}"/></Shape>
-         {% else %}ID="eSc_dummyblock_">{% endif %}
-          {% endifchanged %}
-	  <TextLine ID="{{ line.external_id }}"
-                    {% if line.typology %}TAGREFS="LT{{line.typology.id}}"{% endif %}
-                    {% if line.baseline %}BASELINE="{% alto_points line.baseline %}" {% endif %}
-                    HPOS="{{ line.box.0 }}"
-                    VPOS="{{ line.box.1 }}"
-                    WIDTH="{{ line.width }}"
-                    HEIGHT="{{ line.height }}">
-            {% if line.mask %}<Shape><Polygon POINTS="{% alto_points line.mask %}"/></Shape>{% endif %}
-	    <String CONTENT="{{ line.transcription.0.content }}"
-                    HPOS="{{ line.box.0 }}"
-                    VPOS="{{ line.box.1 }}"
-                    WIDTH="{{ line.width }}"
-                    HEIGHT="{{ line.height }}"></String>
-          </TextLine>
-          {% if forloop.last %}</TextBlock>{% endif %}
+        {% for block in blocks %}
+        <TextBlock HPOS="{{ block.coordinates_box.0 }}"
+                   VPOS="{{ block.coordinates_box.1 }}"
+                   WIDTH="{{ block.width }}"
+                   HEIGHT="{{ block.height }}"
+                   ID="{{ block.external_id }}"
+                   TAGREFS="BT{{block.typology.id}}">
+          <Shape><Polygon POINTS="{% alto_points block.box %}"/></Shape>
+          {% for line in block.lines.all %}
+          {% include "export/alto_line.xml" %}
           {% endfor %}
-        </PrintSpace>
-      </Page>
-    </Layout>
-  </alto>
+        </TextBlock>
+        {% endfor %}
+
+        {% if orphan_lines|length %}
+        <TextBlock ID="eSc_dummyblock_">
+          {% for line in orphan_lines %}
+          {% include "export/alto_line.xml" %}
+          {% endfor %}
+        </TextBlock>
+        {% endif %}
+      </PrintSpace>
+    </Page>
+  </Layout>
+</alto>

app/escriptorium/templates/export/alto_line.xml
@@ -1,0 +1,15 @@
+{% load export_tags %}
+          <TextLine ID="{{ line.external_id }}"
+                    {% if line.typology %}TAGREFS="LT{{line.typology.id}}"{% endif %}
+                    {% if line.baseline %}BASELINE="{% alto_points line.baseline %}" {% endif %}
+                    HPOS="{{ line.box.0 }}"
+                    VPOS="{{ line.box.1 }}"
+                    WIDTH="{{ line.width }}"
+                    HEIGHT="{{ line.height }}">
+            {% if line.mask %}<Shape><Polygon POINTS="{% alto_points line.mask %}"/></Shape>{% endif %}
+	    <String CONTENT="{{ line.transcription.0.content }}"
+                    HPOS="{{ line.box.0 }}"
+                    VPOS="{{ line.box.1 }}"
+                    WIDTH="{{ line.width }}"
+                    HEIGHT="{{ line.height }}"></String>
+          </TextLine>

app/escriptorium/templates/export/pagexml.xml
@@ -6,20 +6,22 @@
     <LastChange>{% current_time %}</LastChange>
   </Metadata>
   <Page imageFilename="{{ part.filename }}" imageWidth="{{ part.image.width  }}" imageHeight="{{ part.image.height  }}">
-    {% for line in lines %}{% ifchanged line.block %}{% if not forloop.first %}</TextRegion>{% endif %}
-    <TextRegion id="{{ line.block.external_id|default:'eSc_dummyblock_' }}"  {% if line.block.typology %}custom="structure {type:{{line.block.typology.name}};}"{% endif %}>
-      <Coords points="{% box_coordinates line.block part %}"/>{% endifchanged %}
-      <TextLine id="{{ line.external_id }}" {% if line.typology %}custom="structure {type:{{line.typology.name}};}"{% endif %}>
-          <Coords points="{% pagexml_points line.mask %}"/>
-        {% if line.baseline %}<Baseline points="{% pagexml_points line.baseline %}"/>{% endif %}
-        <TextEquiv>
-          <Unicode>{{ line.transcription.0.content|default_if_none:"" }}</Unicode>
-        </TextEquiv>
-      </TextLine>
-      {% if forloop.last %}<TextEquiv>
-        <Unicode>{% for line in lines %}{% line_break line.transcription.0.content|default_if_none:"" %}{% endfor %}</Unicode>
-      </TextEquiv>
-    </TextRegion>{% endif %}
+
+    {% for block in blocks %}
+    <TextRegion id="{{ block.external_id }}"  {% if block.typology %}custom="structure {type:{{block.typology.name}};}"{% endif %}>
+      <Coords points="{% pagexml_points block.box %}"/>
+      {% for line in block.lines.all %}
+      {% include 'export/pagexml_line.xml' %}
+      {% endfor %}
+    </TextRegion>
     {% endfor %}
+
+    {% if orphan_lines|length %}
+    <TextRegion id="eSc_dummyblock_">
+    {% for line in orphan_lines %}
+    {% include 'export/pagexml_line.xml' %}
+    {% endfor %}
+    </TextRegion>
+    {% endif %}
   </Page>
 </PcGts>

app/escriptorium/templates/export/pagexml_line.xml
@@ -1,0 +1,8 @@
+{% load export_tags %}
+      <TextLine id="{{ line.external_id }}" {% if line.typology %}custom="structure {type:{{line.typology.name}};}"{% endif %}>
+        {% if line.mask %}<Coords points="{% pagexml_points line.mask %}"/>{% endif %}
+        {% if line.baseline %}<Baseline points="{% pagexml_points line.baseline %}"/>{% endif %}
+        <TextEquiv>
+          <Unicode>{{ line.transcription.0.content|default_if_none:"" }}</Unicode>
+        </TextEquiv>
+      </TextLine>

commit 271ab48ea4bbb6b58d38887569503bbdd02f5bf5

Remove unused export tag 'line_break'
Fixes: c2769aa9a5b9eb58 ("Fixes empty regions not being imported ...")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

app/apps/imports/templatetags/export_tags.py
@@ -10,11 +10,6 @@ def current_time():
 
 
 @register.simple_tag
-def line_break(lines):
-    return "{} \n".format(lines)
-
-
-@register.simple_tag
 def pagexml_points(points):
     return ' '.join(','.join(map(lambda x: str(int(x)), pt)) for pt in points)
 