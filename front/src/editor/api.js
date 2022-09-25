7a315b92a6c851f76f86bf926c63fad2ad866e18
Stefan Weil <sw@weilnetz.de>
2022-09-25 16:48:46 +0200

    Fix code for installation in subpath
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- front/src/editor/api.js
+++ front/src/editor/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = scriptName() + '/api'
+const path_components = location.pathname.split("/", 2);
+const prefix = path_components[1];
+const SCRIPT_NAME = (prefix.length > 0 ? "/" + prefix : "")
+
+axios.defaults.baseURL = SCRIPT_NAME + '/api'
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.withCredentials = true
