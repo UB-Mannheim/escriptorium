29f79f5e99c59cf41f1b5c16537517b906355986
Ben Silverman <ben@performantsoftware.com>
2023-01-04 13:02:14 -0500

    Allow light and dark mode switching in Storybook

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -5,8 +5,14 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      }
+    },
+    "@storybook/addon-interactions",
+    "storybook-addon-themes"
   ],
   "framework": "@storybook/vue",
   "core": {
--- front/.storybook/preview.js
+++ front/.storybook/preview.js
@@ -8,4 +8,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  themes: {
+    clearable: false,
+    default: 'Light mode',
+    list: [
+      { name: 'Light mode', class: 'light-mode', color: '#D3D3D3' },
+      { name: 'Dark mode', class: 'dark-mode', color: '#696969' }
+    ],
+  },
 }
