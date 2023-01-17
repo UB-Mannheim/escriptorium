3ae73a4b9b29aae96b6a409eb54eff60c11e81f3
Robin Tissot <tissotrobin@gmail.com>
2023-01-17 17:28:19 +0100

    Attempt to fix offset region layer issue.

--- front/src/wheelzoom.js
+++ front/src/wheelzoom.js
@@ -235,24 +235,24 @@ export class WheelZoom {
 	// Make sure the slide stays in its container area when zooming in/out
         if (ter.width * this.scale > ts.width) {
             if (this.pos.x > 0) { this.pos.x = 0; }
-            if (this.pos.x + ter.width * this.scale < ts.width) {
-                this.pos.x = ts.width - ter.width * this.scale;
+            if (this.pos.x + ter.width < ts.width) {
+                this.pos.x = ts.width - ter.width;
             }
         } else {
             if (this.pos.x < 0) { this.pos.x = 0; }
-	    if (this.pos.x + ter.width *  this.scale > ts.width) {
-                this.pos.x = ts.width - ter.width *  this.scale;
+	    if (this.pos.x + ter.width > ts.width) {
+                this.pos.x = ts.width - ter.width;
             }
         }
         if (ter.height * this.scale > ts.height) {
             if (this.pos.y > 0) { this.pos.y = 0; }
-	    if (this.pos.y + ter.height * this.scale < ts.height) {
-                this.pos.y = ts.height - ter.height * this.scale;
+	    if (this.pos.y + ter.height < ts.height) {
+                this.pos.y = ts.height - ter.height;
             }
         } else {
             if (this.pos.y < 0) { this.pos.y = 0; }
-            if (this.pos.y + ter.height * this.scale > ts.height) {
-                this.pos.y = ts.height - ter.height *  this.scale;
+            if (this.pos.y + ter.height > ts.height) {
+                this.pos.y = ts.height - ter.height;
             }
         }
 
