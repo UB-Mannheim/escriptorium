f94aa5dddf9725322db719536036d18f6d974ef5
Ben Silverman <ben@performantsoftware.com>
2023-01-19 13:31:45 -0500

    Implement filter button component

--- front/src/stories/TagFilter.stories.js
+++ front/src/stories/TagFilter.stories.js
@@ -1,10 +1,13 @@
 import TagFilter from "../../vue/components/TagFilter/TagFilter.vue";
+import FilterButton from "../../vue/components/FilterButton/FilterButton.vue";
+import TagIcon from "../../vue/components/Icons/TagIcon/TagIcon.vue";
 
 import { ManyTags } from "./Tags.stories";
 
 export default {
     title: "Components/Tag Filter",
     component: TagFilter,
+    subcomponents: { FilterButton, TagIcon },
 };
 
 const Template = (args, { argTypes }) => ({
@@ -18,4 +21,24 @@ export const TagFilterModal = Template.bind({});
 TagFilterModal.args = {
     tags: ManyTags.args.tags,
     selected: ["Third tag", "Fifth tag"],
+};
+
+const WithButtonTemplate = (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { TagFilter, FilterButton, TagIcon },
+    template: `
+        <FilterButton v-bind="$props">
+            <template v-slot:filter-icon="{active}">
+                <TagIcon :active="active" />
+            </template>
+        </FilterButton>
+    `,
+});
+export const TagFilterButton = WithButtonTemplate.bind({});
+TagFilterButton.args = {
+    active: true,
+    count: 2,
+    label: "Tags",
+    onClear: () => {},
+    onClick: () => {},
 };
---
+++ front/vue/components/FilterButton/FilterButton.vue
@@ -0,0 +1,50 @@
+<template>
+    <div :class="classes">
+        <button class="escr-button" @click="onClick">
+            <slot name="filter-icon" :active="active"></slot>
+            <span class="label">{{ label }}</span>
+            <span v-if="active && count" class="count">{{ count }}</span>
+        </button>
+        <button class="escr-clear-filter-button" v-if="active" @click="onClear">
+            <XIcon />
+            <span class="sr-only">Clear filter</span>
+        </button>
+    </div>
+</template>
+<script>
+
+import "../Button/Button.css";
+import XIcon from "../Icons/XIcon/XIcon.vue";
+import "./FilterButton.css";
+
+export default {
+    name: "escr-filter-button",
+    props: {
+        active: {
+            type: Boolean,
+        },
+        count: {
+            type: Number,
+        },
+        label: {
+            type: String,
+        },
+        onClear: {
+            type: Function,
+        },
+        onClick: {
+            type: Function,
+        },
+    },
+    computed: {
+        classes() {
+            return {
+                "escr-filter-button": true,
+                "escr-filter-button--active": this.active,
+            };
+        }
+    },
+    components: { XIcon }
+}
+
+</script>
