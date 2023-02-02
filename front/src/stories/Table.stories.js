5b6dc5aa5deaf801d26c8819b4ef1405c5e93b1b
Ben Silverman <ben@performantsoftware.com>
2023-02-02 16:53:47 -0500

    Simplify sortable table: use only external sort

--- front/src/stories/Table.stories.js
+++ front/src/stories/Table.stories.js
@@ -5,6 +5,11 @@ export default {
     component: Table,
 };
 
+let items = [
+    { id: 1, name: "Project Name", owner: "Ryuichi Sakamoto", updated: "2022-08-09" },
+    { id: 2, name: "Second Project", owner: "Haruomi Hosono", updated: "2023-01-31" },
+    { id: 3, name: "A Third Project", owner: "Yukihiro Takahashi", updated: "2022-01-09" },
+];
 
 const Template = (args, { argTypes }) => ({
     props: Object.keys(argTypes),
@@ -18,23 +23,36 @@ BasicTable.args = {
         { label: "Owner", value: "owner" },
         { label: "Last updated", value: "updated" },
     ],
-    items: [
-        { id: 1, name: "Project Name", owner: "Ryuichi Sakamoto", updated: "2022-08-09" },
-        { id: 2, name: "Second Project", owner: "Haruomi Hosono", updated: "2023-01-31" },
-        { id: 3, name: "A Third Project", owner: "Yukihiro Takahashi", updated: "2022-01-09" },
-    ],
+    items,
     itemKey: "id"
 };
+
+const onSort = (field, direction) => {
+    const alphabeticSort = (key) => (a, b) => {
+        return a[key].toString().localeCompare(b[key].toString());
+    };
+    if (direction === 0) {
+        items.sort(alphabeticSort("id"));
+    } else {
+        items.sort(alphabeticSort(field));
+        if (direction === -1) {
+            items.reverse();
+        }
+    }
+}
 
 export const SortableTable = Template.bind({});
 SortableTable.args = {
     ...BasicTable.args,
+    items,
     headers: BasicTable.args.headers.map((header) => ({ ...header, sortable: true })),
+    onSort,
 };
 
 export const LinkableTable = Template.bind({});
 LinkableTable.args = {
-    ...SortableTable.args,
-    items: SortableTable.args.items.map((item) => ({ ...item, href: window.parent.location })),
+    ...BasicTable.args,
+    items: items.map((item) => ({ ...item, href: window.parent.location })),
     linkable: true,
+    onSort,
 };
--- front/vue/components/Table/Table.vue
+++ front/vue/components/Table/Table.vue
@@ -32,7 +32,7 @@
         </thead>
         <tbody>
             <tr
-                v-for="item in sortedItems"
+                v-for="item in items"
                 :key="item[itemKey]"
             >
                 <td
@@ -68,20 +68,8 @@ export default {
          * List of headers, each of which is an object that should have a `value` and a `label`.
          * To make a column sortable, set the `sortable` boolean for its header to `true`.
          *
-         * Data may be sorted on the frontend (default), or using the callback function prop
-         * `onSort` to make API calls and mutate the `items` prop. (If you use `onSort`, you must
-         * also set the boolean prop `useOnSort` to `true`.)
-         *
-         * For frontend sorting, to define a non-alphabetic sorting algorithm for a column,
-         * include a `sortFn` function. `sortFn` should be a curried function that accepts
-         * a string key as the parameter for the outer function, and a compare function that
-         * compares the values of the two passed items for that key. For example:
-         *
-         * ```js
-         * sortFn: (key) => (a, b) => {
-         *     return a[key].localeCompare(b[key]);
-         * }
-         * ```
+         * When using sortable columns, data may be sorted by using the callback function prop
+         * `onSort` to mutate the `items` prop.
          */
         headers: {
             type: Array,
@@ -118,25 +106,15 @@ export default {
             default: false,
         },
         /**
-         * An optional callback function for sorting, primarily to be used for API calls
-         * to retrieve sorted data from a backend and pass it back to the `items` prop.
+         * A callback function for sorting, which may be used for frontend sorting or for API calls
+         * to retrieve sorted data from a backend, and which should mutate the `items` prop.
          *
          * Must be a function that accepts two params: `field`, a string for the field to sort
          * on, and `direction`, which will be one of the numbers 0, -1, or 1.
-         *
-         * If you want to use this prop, you must also set the `useOnSort` prop to `true`.
          */
         onSort: {
             type: Function,
             default: () => {},
-        },
-        /**
-         * Boolean indicating whether or not to use the `onSort` prop to sort items externally,
-         * rather than on the frontend. Must be set `true` for the `onSort` prop to work.
-         */
-        useOnSort: {
-            type: Boolean,
-            default: false,
         },
     },
     data() {
@@ -146,36 +124,10 @@ export default {
             },
         };
     },
-    computed: {
-        /**
-         * Get a copy of the items array, sorted by the selected column (if sort is
-         * active) using either alphanumeric sort, or a supplied sort function.
-         *
-         * If `useOnSort` prop is set, just return the `items` array, since `onSort` is meant
-         * to make an API call to mutate the original `items` array passed as a prop.
-         */
-        sortedItems() {
-            if (!this.useOnSort && this.sortState.value && this.sortState.direction !== 0) {
-                const alphabeticSort = (key) => (a, b) => {
-                    return a[key].localeCompare(b[key]);
-                };
-                const sorted = [...this.items].sort(
-                    this.sortState.sortFn
-                        ? this.sortState.sortFn(this.sortState.value)
-                        : alphabeticSort(this.sortState.value)
-                );
-                return this.sortState.direction === 1 ? sorted : sorted.reverse();
-            } else {
-                return this.items;
-            }
-        }
-    },
     methods: {
         /**
          * When a header is clicked to change the sort state, update the sort state
-         * accordingly.
-         *
-         * If the `onSort` prop is set, call it to make any API calls.
+         * accordingly and call the `onSort` callback prop.
          */
         setSort(header) {
             const { value, direction } = this.sortState;
@@ -188,9 +140,7 @@ export default {
                 direction: newDirection,
                 sortFn: this.sortState.sortFn || undefined,
             };
-            if (this.useOnSort) {
-                this.onSort(header.value, newDirection);
-            }
+            this.onSort(header.value, newDirection);
         },
     },
 }
