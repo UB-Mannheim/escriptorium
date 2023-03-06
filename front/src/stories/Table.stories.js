1a7c748dc18be6fdb4c282ddf1c9b82aede7e462
Ben Silverman <ben@performantsoftware.com>
2023-03-06 17:29:50 -0500

    Factor out more reused sorting in storybook

--- front/src/stories/Table.stories.js
+++ front/src/stories/Table.stories.js
@@ -1,6 +1,7 @@
 import Button from "../../vue/components/Button/Button.vue";
 import Table from "../../vue/components/Table/Table.vue";
 import TrashIcon from "../../vue/components/Icons/TrashIcon/TrashIcon.vue";
+import { onSort } from "./util";
 
 export default {
     title: "Components/Table",
@@ -8,9 +9,9 @@ export default {
 };
 
 let items = [
-    { id: 1, name: "Project Name", owner: "Ryuichi Sakamoto", updated: "2022-08-09" },
-    { id: 2, name: "Second Project", owner: "Haruomi Hosono", updated: "2023-01-31" },
-    { id: 3, name: "A Third Project", owner: "Yukihiro Takahashi", updated: "2022-01-09" },
+    { pk: 1, name: "Project Name", owner: "Ryuichi Sakamoto", updated: "2022-08-09" },
+    { pk: 2, name: "Second Project", owner: "Haruomi Hosono", updated: "2023-01-31" },
+    { pk: 3, name: "A Third Project", owner: "Yukihiro Takahashi", updated: "2022-01-09" },
 ];
 
 const Template = (args, { argTypes }) => ({
@@ -28,20 +29,6 @@ BasicTable.args = {
     items,
     itemKey: "id"
 };
-
-const onSort = ({ field, direction }) => {
-    const alphabeticSort = (key) => (a, b) => {
-        return a[key].toString().localeCompare(b[key].toString());
-    };
-    if (direction === 0) {
-        items.sort(alphabeticSort("id"));
-    } else {
-        items.sort(alphabeticSort(field));
-        if (direction === -1) {
-            items.reverse();
-        }
-    }
-}
 
 export const SortableTable = Template.bind({});
 SortableTable.args = {
--- front/src/stories/util.js
+++ front/src/stories/util.js
@@ -1,11 +1,13 @@
+// sort utility functions
+const alphabeticSort = (key) => (a, b) => {
+    return a[key].toString().localeCompare(b[key].toString());
+};
+const numericSort = (key) => (a, b) => {
+    return a[key] - b[key];
+};
+
 // utility function for sorting lists in mock API responses
 export const sorted = (items, { ordering }) => {
-    const alphabeticSort = (key) => (a, b) => {
-        return a[key].toString().localeCompare(b[key].toString());
-    };
-    const numericSort = (key) => (a, b) => {
-        return a[key] - b[key];
-    };
     if (!ordering) {
         return [...items].sort(numericSort("id"));
     } else {
@@ -24,3 +26,36 @@ export const sorted = (items, { ordering }) => {
         return sorted;
     }
 };
+
+// utility function to sort in place when we aren't using API
+export const onSort = (items, { field, direction }) => {
+    if (direction === 0) {
+        items.sort(numericSort("pk"));
+    } else if (field.includes("count")) {
+        items.sort(numericSort(field));
+    } else {
+        items.sort(alphabeticSort(field));
+    }
+    if (direction === -1) {
+        items.reverse();
+    }
+}
+
+// mock ontologies for all stories with ontology
+export const blockTypes = [
+    { pk: 1, name: "Main", count: 2 },
+    { pk: 2, name: "Commentary", count: 3 },
+    { pk: 3, name: "Header", count: 1 },
+];
+export const lineTypes = [
+    { pk: 1, name: "Correction", count: 13 },
+    { pk: 2, name: "Main", count: 101 },
+];
+export const annotationTypes = [
+    { pk: 1, name: "Quote", count: 4 },
+    { pk: 2, name: "Translation", count: 11 },
+];
+export const partTypes = [
+    { pk: 1, name: "Cover", count: 4 },
+    { pk: 2, name: "Page", count: 100 },
+];
