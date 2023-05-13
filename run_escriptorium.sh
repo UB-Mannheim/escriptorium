c2a7f77c4d30de853c9c807f7a248026fb333868
Stefan Weil <sw@weilnetz.de>
2023-05-13 18:06:59 +0200

    Run only a single OMP thread and increase concurrency to 4
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -4,5 +4,7 @@ source /home/stweil/src/gitlab/scripta/escriptorium/venv3.9/bin/activate
 export DJANGO_SETTINGS_MODULE=escriptorium.local_settings
 export ESC_LANGUAGES="de,en,fr"
 
-celery worker --app escriptorium --concurrency 2 --loglevel INFO &
+export OMP_NUM_THREADS=1
+
+celery worker --app escriptorium --concurrency 4 --loglevel INFO &
 python manage.py runserver --settings escriptorium.local_settings --verbosity 2 0.0.0.0:8080
