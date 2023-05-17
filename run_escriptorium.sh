1266072381f197bcfaa31ad9d3dbfd6713372647
Stefan Weil <sw@weilnetz.de>
2023-05-17 04:06:00 +0200

    Update start script (number of threads and workers)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -4,7 +4,7 @@ source /home/stweil/src/gitlab/scripta/escriptorium/venv3.9/bin/activate
 export DJANGO_SETTINGS_MODULE=escriptorium.local_settings
 export ESC_LANGUAGES="de,en,fr"
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=4
 
-celery worker --app escriptorium --concurrency 4 --loglevel INFO &
+celery worker --app escriptorium --concurrency 8 --loglevel INFO &
 python manage.py runserver --settings escriptorium.local_settings --verbosity 2 0.0.0.0:8080
