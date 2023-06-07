495f8f3e99376c0684c4e54f35d688954235bb3b
Ben Silverman <ben@performantsoftware.com>
2023-06-07 14:33:00 -0400

    Add throttle/last dispatched action to vue store

--- front/vue/store/index.js
+++ front/vue/store/index.js
@@ -31,8 +31,15 @@ Vue.use(FloatingVue, {
     },
 });
 
-export default new Store({
+const store = new Store({
     strict: process.env.NODE_ENV !== "production",
+    mutations: {
+        // update last dispatched action
+        // (from https://upstatement.com/blog/last-vuex-action-hero/)
+        LAST_DISPATCHED_ACTION: (state, actionObject) => {
+            state.lastDispatchedAction = actionObject;
+        },
+    },
     modules: {
         alerts,
         characters,
@@ -48,3 +55,19 @@ export default new Store({
         user,
     },
 });
+
+// keep track of last dispatched action (from https://upstatement.com/blog/last-vuex-action-hero/)
+// Store the initial instance of the store's dispatch function
+const initialDispatch = store.dispatch;
+
+// overwrite `dispatch` on store so that it additionally tracks
+// which function was called last
+// this is used so we can retry any given function if auth has expired
+Object.defineProperty(store, "dispatch", {
+    value(name, payload) {
+        store.commit("LAST_DISPATCHED_ACTION", { name, payload });
+        return initialDispatch(...arguments);
+    },
+});
+
+export default store;
