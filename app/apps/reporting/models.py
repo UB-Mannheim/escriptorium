2f38ed4d89184e0bb4383782e629361b98a1f6d9
Stefan Weil <sw@weilnetz.de>
2023-06-07 16:32:36 +0200

    Replace celery.task.control
    Python 3.11 requires newer versions of the celery package.
    Those versions removed celery.task.control which was deprecated
    since celery 4.0.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/apps/reporting/models.py
+++ app/apps/reporting/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
 
-from celery.task.control import revoke
+# from celery.app import control
+from celery import app
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
@@ -81,7 +83,7 @@ class TaskReport(models.Model):
             canceled_by = f"user {username}"
         self.append(f"Canceled by {canceled_by}")
 
-        revoke(self.task_id, terminate=True)
+        app.control.revoke(self.task_id, terminate=True)
         self.save()
 
     def error(self, message):
