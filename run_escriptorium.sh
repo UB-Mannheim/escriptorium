f5fed2b32b75b610f63479ee171ad7282d6e5ffc
Stefan Weil <sw@weilnetz.de>
2023-06-09 16:14:48 +0200

    Update start script (more workers)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -7,5 +7,5 @@ export ESC_LANGUAGES="de,en,fr"
 export OMP_NUM_THREADS=1
 
 cd /home/stweil/src/gitlab/scripta/escriptorium/app
-celery worker --app escriptorium --concurrency 2 --loglevel DEBUG &
+celery worker --app escriptorium --concurrency 16 --loglevel DEBUG &
 python -Wa manage.py runserver --settings escriptorium.local_settings --verbosity 3 0.0.0.0:8080
