9711479e844fc84b8422038d543a4754d51c6406
Stefan Weil <sw@weilnetz.de>
2023-06-09 16:36:30 +0200

    Update start script
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -3,7 +3,7 @@
 source /home/stweil/src/gitlab/scripta/escriptorium/venv3.9/bin/activate
 export DJANGO_SETTINGS_MODULE=escriptorium.local_settings
 export ESC_LANGUAGES="de,en,fr"
-
+export LANG=C.UTF-8
 export OMP_NUM_THREADS=4
 
 celery worker --app escriptorium --concurrency 8 --loglevel INFO &
