a269d5153715595b4895759c7401f285c7a33d44
Stefan Weil <sw@weilnetz.de>
2023-06-14 18:24:46 +0200

    Update startup script for Python 3.11
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source /home/stweil/src/gitlab/scripta/escriptorium/venv/bin/activate
+source /home/stweil/src/gitlab/scripta/venv3.11/bin/activate
 export DJANGO_SETTINGS_MODULE=escriptorium.local_settings
 export ESC_LANGUAGES="de,en,fr"
 
 export OMP_NUM_THREADS=1
 
 cd /home/stweil/src/gitlab/scripta/escriptorium/app
-celery worker --app escriptorium --concurrency 16 --loglevel DEBUG &
+celery --app escriptorium worker --concurrency 16 --loglevel DEBUG &
 python -Wa manage.py runserver --settings escriptorium.local_settings --verbosity 3 0.0.0.0:8080
