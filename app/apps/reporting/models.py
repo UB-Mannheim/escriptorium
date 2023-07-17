8487bf0f8a02c26793ab185abd0285e25c908628
Robin Tissot <tissotrobin@gmail.com>
2023-07-17 15:45:43 +0200

    isort imports.

--- app/apps/reporting/models.py
+++ app/apps/reporting/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
 from escriptorium.celery import app
 
 User = get_user_model()
