949b46d230e4821ad9e42e91406e8d73914e9d41
Ben Silverman <ben@performantsoftware.com>
2023-08-23 16:25:10 -0400

    ESLint auto-fix all Vue components

--- front/vue/components/TagsManagement.vue
+++ front/vue/components/TagsManagement.vue
@@ -1,27 +1,97 @@
 <template>
-    <div class="modal fade" id="tagsManagement" ref="tagsManagement" tabindex="-1" role="dialog" aria-labelledby="tagsManagementLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-xl" id="modaltag" role="document">
+    <div
+        id="tagsManagement"
+        ref="tagsManagement"
+        class="modal fade"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="tagsManagementLabel"
+        aria-hidden="true"
+    >
+        <div
+            id="modaltag"
+            class="modal-dialog modal-dialog-centered modal-xl"
+            role="document"
+        >
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Tags management</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <h5 class="modal-title">
+                        Tags management
+                    </h5>
+                    <button
+                        type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                    >
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div method="post" action="#" ref="formManageTag">
-                    <input type="hidden" class="form-control" id="project-id" name="project" v-model="projectIdComputed">
+                <div
+                    ref="formManageTag"
+                    method="post"
+                    action="#"
+                >
+                    <input
+                        id="project-id"
+                        v-model="projectIdComputed"
+                        type="hidden"
+                        class="form-control"
+                        name="project"
+                    >
                     <div class="modal-body row mb-0 pb-0">
                         <div class="form-group col-sm-4 d-inline-flex mx-auto">
-                            <input type="text" class="form-control m-1" @focusout="updateSingleTag()" ref="tagName" placeholder="Type to add a tag">
-                            <input type="color" class="form-control m-1 w-25" v-bind:value="tagColor" @focusout="updateSingleTag()" ref="tagColor">
+                            <input
+                                ref="tagName"
+                                type="text"
+                                class="form-control m-1"
+                                placeholder="Type to add a tag"
+                                @focusout="updateSingleTag()"
+                            >
+                            <input
+                                ref="tagColor"
+                                type="color"
+                                class="form-control m-1 w-25"
+                                :value="tagColor"
+                                @focusout="updateSingleTag()"
+                            >
                         </div>
                     </div>
                     <div class="modal-body row">
-                        <div class="form-group col-sm-4 d-inline-flex" v-for="tag in tags" :key="tag.pk" v-bind:value="tag.pk" v-bind:id="'div-tag-' + tag.pk">
-                            <input type="checkbox" v-bind:id="'checkbox-tag-' + tag.pk" v-on:click="assignSingleTag(tag.pk)" title="Assign this tag to selected documents.">
-                            <input type="text" class="form-control m-1" v-bind:id="'name-tag-' + tag.pk" v-bind:value="tag.name" @focusout="updateSingleTag(tag.pk)">
-                            <input type="color" class="form-control m-1 w-25" v-bind:id="'color-tag-' + tag.pk" v-bind:value="tag.color" @focusout="updateSingleTag(tag.pk)">
-                            <button type="button" class="btn btn-danger m-1" v-on:click="deleteSingleTag(tag.pk)"><i class="fas fa-trash"></i></button>
+                        <div
+                            v-for="tag in tags"
+                            :id="'div-tag-' + tag.pk"
+                            :key="tag.pk"
+                            class="form-group col-sm-4 d-inline-flex"
+                            :value="tag.pk"
+                        >
+                            <input
+                                :id="'checkbox-tag-' + tag.pk"
+                                type="checkbox"
+                                title="Assign this tag to selected documents."
+                                @click="assignSingleTag(tag.pk)"
+                            >
+                            <input
+                                :id="'name-tag-' + tag.pk"
+                                type="text"
+                                class="form-control m-1"
+                                :value="tag.name"
+                                @focusout="updateSingleTag(tag.pk)"
+                            >
+                            <input
+                                :id="'color-tag-' + tag.pk"
+                                type="color"
+                                class="form-control m-1 w-25"
+                                :value="tag.color"
+                                @focusout="updateSingleTag(tag.pk)"
+                            >
+                            <button
+                                type="button"
+                                class="btn btn-danger m-1"
+                                @click="deleteSingleTag(tag.pk)"
+                            >
+                                <i class="fas fa-trash" />
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -33,7 +103,7 @@
 <script>
 export default {
     props: [
-        'projectId',
+        "projectId",
     ],
     data () {
         return {
@@ -48,6 +118,15 @@ export default {
             return this.$store.state.documentslist.tagColor;
         }
     },
+    watch: {
+        "$store.state.documentslist.mapTags": {
+            handler: function(nv) {
+                this.tags = nv;
+                this.$store.commit("documentslist/setTagColor");
+            },
+            immediate: true
+        }
+    },
     mounted(){
         $(this.$refs.tagsManagement).on("show.bs.modal", this.populateItems);
     },
@@ -55,36 +134,27 @@ export default {
         async updateSingleTag(pk=null){
             let _name = ($("#name-tag-" + pk).val()) ? $("#name-tag-" + pk).val() : $(this.$refs.tagName).val();
             if(_name){
-                if(pk) await this.$store.dispatch('documentslist/updateProjectTag', {pk: pk, name: _name, color: $("#color-tag-" + pk).val()});
+                if(pk) await this.$store.dispatch("documentslist/updateProjectTag", {pk: pk, name: _name, color: $("#color-tag-" + pk).val()});
                 else{
-                    await this.$store.dispatch('documentslist/updateDocumentTags', {name: _name, color: $(this.$refs.tagColor).val()});
+                    await this.$store.dispatch("documentslist/updateDocumentTags", {name: _name, color: $(this.$refs.tagColor).val()});
                     this.resetField();
                 }
             }
         },
         async deleteSingleTag(pk){
-            await this.$store.dispatch('documentslist/deleteProjectTag', {pk: pk});
+            await this.$store.dispatch("documentslist/deleteProjectTag", {pk: pk});
             $("#div-tag-" + pk).remove();
         },
         async assignSingleTag(pk){
-            await this.$store.dispatch('documentslist/assignSingleTagToDocuments', {pk: pk, selected: $('#checkbox-tag-' + pk).prop('checked')});
+            await this.$store.dispatch("documentslist/assignSingleTagToDocuments", {pk: pk, selected: $("#checkbox-tag-" + pk).prop("checked")});
         },
         async populateItems(){
-            this.$store.commit('documentslist/setProjectID', this.projectId);
-            this.$store.commit('documentslist/setTagColor');
-            await this.$store.dispatch('documentslist/getAllTagsProject');
+            this.$store.commit("documentslist/setProjectID", this.projectId);
+            this.$store.commit("documentslist/setTagColor");
+            await this.$store.dispatch("documentslist/getAllTagsProject");
         },
         resetField(){
             $(this.$refs.tagName).val("");
-        }
-    },
-    watch: {
-        "$store.state.documentslist.mapTags": {
-            handler: function(nv) {
-                this.tags = nv;
-                this.$store.commit('documentslist/setTagColor');
-            },
-            immediate: true
         }
     }
 }
--- front/vue/components/VisuLine.vue
+++ front/vue/components/VisuLine.vue
@@ -1,63 +1,135 @@
 <template>
-    <g @mouseover="showOverlay"
+    <g
+        @mouseover="showOverlay"
         @mouseleave="hideOverlay"
-        @click="edit">
-        <polygon    v-bind:fill="$store.state.document.confidenceVisible ? maskFillColor : 'transparent'"
-                    v-bind:stroke="maskStrokeColor"
-                    v-bind:points="maskPoints"/>
-        <path v-bind:id="textPathId"
-                ref="pathElement"
-                fill="none"
-                v-bind:stroke="pathStrokeColor"
-                v-bind:d="baselinePoints"></path>
+        @click="edit"
+    >
+        <polygon
+            :fill="$store.state.document.confidenceVisible ? maskFillColor : 'transparent'"
+            :stroke="maskStrokeColor"
+            :points="maskPoints"
+        />
+        <path
+            :id="textPathId"
+            ref="pathElement"
+            fill="none"
+            :stroke="pathStrokeColor"
+            :d="baselinePoints"
+        />
 
-        <text :text-anchor="$store.state.document.defaultTextDirection == 'rtl' ? 'end' : ''"
-                ref="textElement"
-                lengthAdjust="spacingAndGlyphs"
-                data-toggle="tooltip"
-                v-if="$store.state.document.mainTextDirection != 'ttb'">
-            <textPath v-bind:href="'#' + textPathId"
-                        v-if="line.currentTrans">
+        <text
+            v-if="$store.state.document.mainTextDirection != 'ttb'"
+            ref="textElement"
+            :text-anchor="$store.state.document.defaultTextDirection == 'rtl' ? 'end' : ''"
+            lengthAdjust="spacingAndGlyphs"
+            data-toggle="tooltip"
+        >
+            <textPath
+                v-if="line.currentTrans"
+                :href="'#' + textPathId"
+            >
                 {{ line.currentTrans.content }}
             </textPath>
         </text>
 
-        <text :text-anchor="$store.state.document.defaultTextDirection == 'rtl' ? 'end' : ''"
-                ref="textElement"
-                writing-mode="tb"
-                font-size="1em"
-                data-toggle="tooltip"
-                v-else>
-            <textPath v-bind:href="'#' + textPathId"
-                        v-if="line.currentTrans">
+        <text
+            v-else
+            ref="textElement"
+            :text-anchor="$store.state.document.defaultTextDirection == 'rtl' ? 'end' : ''"
+            writing-mode="tb"
+            font-size="1em"
+            data-toggle="tooltip"
+        >
+            <textPath
+                v-if="line.currentTrans"
+                :href="'#' + textPathId"
+            >
                 {{ line.currentTrans.content }}
             </textPath>
         </text>
-
     </g>
 </template>
 
 <script>
-import { LineBase } from '../../src/editor/mixins.js';
+import { LineBase } from "../../src/editor/mixins.js";
 
 export default Vue.extend({
     mixins: [LineBase],
-    mounted() {
-        this.$nextTick(this.reset);
+    computed: {
+        textPathId() {
+            return this.line ? "textPath"+this.line.pk : "";
+        },
+        maskStrokeColor() {
+            if (this.line.currentTrans && this.line.currentTrans.content) {
+                return "none";
+            } else {
+                return "lightgrey";
+            }
+        },
+        lineAvgConfidence() {
+            // compute the average confidence for the current line
+            if (this.line.currentTrans.avg_confidence) {
+                return this.line.currentTrans.avg_confidence;
+            } else if (this.line.currentTrans?.graphs?.length) {
+                const lineConfidences = this.line.currentTrans.graphs.map((g) => g.confidence);
+                return lineConfidences.reduce((all, one, _, src) => all += one / src.length, 0);
+            }
+        },
+        maskFillColor() {
+            if (this.line.currentTrans?.graphs?.length || this.line.currentTrans?.avg_confidence) {
+                // convert the avg confidence to hue (0 = red, 120 = green)
+                // use a slight curve so that values are more easily red/yellow
+                const hue = Math.pow(this.lineAvgConfidence, this.$store.state.document.confidenceScale) * 120;
+                return `hsl(${hue}, 100%, 50%, 50%)`;
+            }
+            return "transparent";
+        },
+        maskPoints() {
+            if (this.line == null || !this.line.mask) return "";
+            return this.line.mask.map((pt) => Math.round(pt[0]*this.ratio)+","+Math.round(pt[1]*this.ratio)).join(" ");
+        },
+        fakeBaseline() {
+            // create a fake path based on the mask,
+            var min = this.line.mask.reduce((minPt, curPt) => (curPt[0] < minPt[0]) ? curPt : minPt);
+            var max = this.line.mask.reduce((maxPt, curPt) => (curPt[0] > maxPt[0]) ? curPt : maxPt);
+            return [min, max];
+        },
+        pathStrokeColor() {
+            if (this.line.currentTrans && this.line.currentTrans.content) {
+                return "none";
+            } else {
+                return "blue";
+            }
+        },
+        baselinePoints() {
+            var baseline, ratio = this.ratio;
+            function ptToStr(pt) {
+                return Math.round(pt[0]*ratio)+" "+Math.round(pt[1]*ratio);
+            }
+            if (this.line == null || this.line.baseline === null) {
+                baseline = this.fakeBaseline;
+            } else {
+                baseline = this.line.baseline
+            }
+            return "M "+baseline.map((pt) => ptToStr(pt)).join(" L ");
+        },
     },
     watch: {
-        'line.currentTrans.content': function(n, o) {
+        "line.currentTrans.content": function(n, o) {
             this.$nextTick(this.reset);
         },
-        'line.baseline': function(n, o) {
+        "line.baseline": function(n, o) {
             this.$nextTick(this.reset);
         }
+    },
+    mounted() {
+        this.$nextTick(this.reset);
     },
     methods: {
         computeLineHeight() {
             let lineHeight;
             if (this.line.mask) {
-                let poly = this.line.mask.flat(1).map(pt => Math.round(pt));
+                let poly = this.line.mask.flat(1).map((pt) => Math.round(pt));
                 var area = 0;
                 // A = 1/2(x_1y_2-x_2y_1+x_2y_3-x_3y_2+...+x_(n-1)y_n-x_ny_(n-1)+x_ny_1-x_1y_n),
 
@@ -92,10 +164,10 @@ export default Vue.extend({
 
             let ratio = 1/4;
 
-            this.$refs.textElement.setAttribute("font-size", String(lineHeight * (ratio)) + 'px');
+            this.$refs.textElement.setAttribute("font-size", String(lineHeight * (ratio)) + "px");
 
             //return lineHeight+'px';
-            return 10+'px';
+            return 10+"px";
         },
         computeTextLength() {
             if (!this.line.currentTrans) return;
@@ -105,7 +177,7 @@ export default Vue.extend({
                 let textLength = this.$refs.textElement.getComputedTextLength();
                 let pathLength = this.$refs.pathElement.getTotalLength();
                 if (textLength && pathLength) {
-                    this.$refs.textElement.setAttribute('textLength', pathLength+'px');
+                    this.$refs.textElement.setAttribute("textLength", pathLength+"px");
                 }
             }
         },
@@ -114,80 +186,21 @@ export default Vue.extend({
             if (this.line.currentTrans?.graphs?.length || this.line.currentTrans?.avg_confidence) {
                 const confidence =  `Confidence: ${(this.lineAvgConfidence * 100).toFixed(1)}%`;
                 // add confidence to bootstrap title related attributes
-                this.$refs.textElement.setAttribute('title' ,confidence);
-                this.$refs.textElement.setAttribute('data-original-title', confidence);
+                this.$refs.textElement.setAttribute("title" ,confidence);
+                this.$refs.textElement.setAttribute("data-original-title", confidence);
             } else {
                 // remove confidence from title attributes
-                this.$refs.textElement.setAttribute('title', '');
-                this.$refs.textElement.setAttribute('data-original-title', '');
+                this.$refs.textElement.setAttribute("title", "");
+                this.$refs.textElement.setAttribute("data-original-title", "");
             }
         },
         edit() {
-            this.$store.dispatch('lines/toggleLineEdition', this.line);
+            this.$store.dispatch("lines/toggleLineEdition", this.line);
         },
         reset() {
             this.computeLineHeight();
             this.computeTextLength();
             this.computeConfidence();
-        },
-    },
-    computed: {
-        textPathId() {
-            return this.line ? 'textPath'+this.line.pk : '';
-        },
-        maskStrokeColor() {
-            if (this.line.currentTrans && this.line.currentTrans.content) {
-                return 'none';
-            } else {
-                return 'lightgrey';
-            }
-        },
-        lineAvgConfidence() {
-            // compute the average confidence for the current line
-            if (this.line.currentTrans.avg_confidence) {
-                return this.line.currentTrans.avg_confidence;
-            } else if (this.line.currentTrans?.graphs?.length) {
-                const lineConfidences = this.line.currentTrans.graphs.map(g => g.confidence);
-                return lineConfidences.reduce((all, one, _, src) => all += one / src.length, 0);
-            }
-        },
-        maskFillColor() {
-            if (this.line.currentTrans?.graphs?.length || this.line.currentTrans?.avg_confidence) {
-                // convert the avg confidence to hue (0 = red, 120 = green)
-                // use a slight curve so that values are more easily red/yellow
-                const hue = Math.pow(this.lineAvgConfidence, this.$store.state.document.confidenceScale) * 120;
-                return `hsl(${hue}, 100%, 50%, 50%)`;
-            }
-            return 'transparent';
-        },
-        maskPoints() {
-            if (this.line == null || !this.line.mask) return '';
-            return this.line.mask.map(pt => Math.round(pt[0]*this.ratio)+','+Math.round(pt[1]*this.ratio)).join(' ');
-        },
-        fakeBaseline() {
-            // create a fake path based on the mask,
-            var min = this.line.mask.reduce((minPt, curPt) => (curPt[0] < minPt[0]) ? curPt : minPt);
-            var max = this.line.mask.reduce((maxPt, curPt) => (curPt[0] > maxPt[0]) ? curPt : maxPt);
-            return [min, max];
-        },
-        pathStrokeColor() {
-            if (this.line.currentTrans && this.line.currentTrans.content) {
-                return 'none';
-            } else {
-                return 'blue';
-            }
-        },
-        baselinePoints() {
-            var baseline, ratio = this.ratio;
-            function ptToStr(pt) {
-                return Math.round(pt[0]*ratio)+' '+Math.round(pt[1]*ratio);
-            }
-            if (this.line == null || this.line.baseline === null) {
-                baseline = this.fakeBaseline;
-            } else {
-                baseline = this.line.baseline
-            }
-            return 'M '+baseline.map(pt => ptToStr(pt)).join(' L ');
         },
     }
 });
