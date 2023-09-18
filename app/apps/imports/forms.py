UB-Mannheim/escriptorium

commit 64362c843de659e5780aeab248611e1dff4c5367

Squashed commits. Adds an import endpoint.

app/apps/imports/forms.py
@@ -20,6 +20,10 @@ from imports.tasks import document_export, document_import
 from users.consumers import send_event
 
 
+class FileImportError(Exception):
+    pass
+
+
 def clean_uri(uri, document, tempfile):
     try:
         resp = requests.get(uri)
@@ -28,17 +32,40 @@ def clean_uri(uri, document, tempfile):
         buf.name = tempfile
         parser = make_parser(document, buf)
         parser.validate()
-        total = parser.total
-        return content, total
+        return content, parser.total
     except requests.exceptions.RequestException:
-        raise forms.ValidationError(_("The document is unreachable, unreadable or the host timed out."))
+        raise FileImportError(_("The document is unreachable, unreadable or the host timed out."))
     except json.decoder.JSONDecodeError:
-        raise forms.ValidationError(_("The document pointed to by the given uri doesn't seem to be valid json."))
+        raise FileImportError(_("The document pointed to by the given uri doesn't seem to be valid json."))
     except ParseError as e:
         msg = _("Couldn't parse the given file or its validation failed")
         if len(e.args):
             msg += ": %s" % e.args[0]
-        raise forms.ValidationError(msg)
+        raise FileImportError(msg)
+
+
+def clean_import_uri(uri, document, tmpFileName):
+    domain = urlparse(uri).netloc
+    if ('*' not in settings.IMPORT_ALLOWED_DOMAINS and domain not in settings.IMPORT_ALLOWED_DOMAINS):
+        raise FileImportError(_("You're not allowed to import files from this domain, please contact your instance administrator."))
+
+    return clean_uri(uri, document, tmpFileName)
+
+
+def clean_upload_file(upload_file, document, user):
+    try:
+        # If quotas are enforced, define if the user can upload ZIP and PDF files
+        allowed = settings.DISABLE_QUOTAS or user.has_free_disk_storage()
+        parser = make_parser(document, upload_file, zip_allowed=allowed, pdf_allowed=allowed)
+        parser.validate()
+    except ParseError as e:
+        msg = _("Couldn't parse the given file or its validation failed")
+        if len(e.args):
+            msg += ": %s" % e.args[0]
+        raise FileImportError(msg)
+    except ValueError as e:
+        raise FileImportError(repr(e))
+    return parser
 
 
 class ImportForm(BootstrapFormMixin, forms.Form):
@@ -80,45 +107,35 @@ class ImportForm(BootstrapFormMixin, forms.Form):
             self.fields['upload_file'].help_text = _("A single ALTO or PAGE XML file.")
 
     def clean_iiif_uri(self):
-        uri = self.cleaned_data.get('iiif_uri')
-        if uri:
-            domain = urlparse(uri).netloc
-            if '*' not in settings.IMPORT_ALLOWED_DOMAINS and domain not in settings.IMPORT_ALLOWED_DOMAINS:
-                raise forms.ValidationError(_("You're not allowed to import files from this domain, please contact your instance administrator."))
-
-            content, total = clean_uri(uri, self.document, 'tmp.json')
-            self.cleaned_data['total'] = total
-            return content
+        try:
+            uri = self.cleaned_data.get('uri')
+            if uri:
+                parser = clean_import_uri(uri, self.document, 'tmp.json')
+                self.cleaned_data['total'] = parser.total
+                return parser.file
+        except FileImportError as e:
+            raise forms.ValidationError(repr(e))
 
     def clean_mets_uri(self):
-        uri = self.cleaned_data.get('mets_uri')
-        self.mets_uri = os.path.dirname(uri)
-        if uri:
-            domain = urlparse(uri).netloc
-            if '*' not in settings.IMPORT_ALLOWED_DOMAINS and domain not in settings.IMPORT_ALLOWED_DOMAINS:
-                raise forms.ValidationError(_("You're not allowed to import files from this domain, please contact your instance administrator."))
-
-            content, total = clean_uri(uri, self.document, 'tmp.xml')
-            self.cleaned_data['total'] = total
-            return content
+        try:
+            uri = self.cleaned_data.get('mets_uri')
+            self.mets_uri = os.path.dirname(uri)
+            if uri:
+                parser = clean_import_uri(uri, self.document, 'tmp.xml')
+                self.cleaned_data['total'] = parser.total
+                return parser.file
+        except FileImportError as e:
+            raise forms.ValidationError(repr(e))
 
     def clean_upload_file(self):
         upload_file = self.cleaned_data.get('upload_file')
         if upload_file:
             try:
-                # If quotas are enforced, define if the user can upload ZIP and PDF files
-                allowed = settings.DISABLE_QUOTAS or self.user.has_free_disk_storage()
-                parser = make_parser(self.document, upload_file, zip_allowed=allowed, pdf_allowed=allowed)
-                parser.validate()
+                parser = clean_upload_file(upload_file, self.document, self.user)
                 self.cleaned_data['total'] = parser.total
-            except ParseError as e:
-                msg = _("Couldn't parse the given file or its validation failed")
-                if len(e.args):
-                    msg += ": %s" % e.args[0]
-                raise forms.ValidationError(msg)
-            except ValueError as e:
-                raise forms.ValidationError(e)
-            return upload_file
+                return parser.file
+            except FileImportError as e:
+                raise forms.ValidationError(repr(e))
 
     def clean(self):
         cleaned_data = super().clean()

app/apps/api/serializers.py
@@ -3,6 +3,7 @@ import logging
 
 import bleach
 from django.conf import settings
+from django.core.files.base import ContentFile
 from django.db.models import Count, Q
 from django.db.utils import IntegrityError
 from django.utils import timezone
@@ -39,6 +40,9 @@ from core.models import (
     Transcription,
 )
 from core.tasks import segment, segtrain, train, transcribe
+from imports.forms import FileImportError, clean_import_uri, clean_upload_file
+from imports.models import DocumentImport
+from imports.tasks import document_import
 from reporting.models import TaskReport
 from users.consumers import send_event
 from users.models import Group, User
@@ -357,6 +361,112 @@ class DocumentSerializer(serializers.ModelSerializer):
             return Script.objects.get(name=value)
         except Script.DoesNotExist:
             raise serializers.ValidationError('This script does not exists in the database.')
+
+
+class ImportSerializer(serializers.Serializer):
+    MODE_CHOICES = (
+        ('pdf', 'Import a pdf file.'),
+        ('iiif', 'Import from a iiif manifest.'),
+        ('mets', 'Import a mets file.'),
+        ('xml', 'Import from a xml file.')
+    )
+    mode = serializers.ChoiceField(choices=MODE_CHOICES)
+
+    transcription = serializers.PrimaryKeyRelatedField(
+        queryset=Transcription.objects.all(),
+        required=False)
+    # override = serializers.BooleanField(allow_blank=True)
+    upload_file = serializers.FileField(required=False)
+
+    iiif_uri = serializers.URLField(required=False)
+
+    METS_TYPE_CHOICES = (
+        ('local', _('Upload local file')),
+        ('url', _('download file from url')),
+    )
+    mets_type = serializers.ChoiceField(choices=METS_TYPE_CHOICES, required=False)
+    mets_uri = serializers.URLField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = self.context["view"].request.user
+        self.document = Document.objects.get(pk=self.context["view"].kwargs["document_pk"])
+        self.fields['transcription'].queryset = Transcription.objects.filter(document=self.document)
+
+    def validate_iiif_uri(self, uri):
+        try:
+            content, total = clean_import_uri(uri, self.document, 'tmp.json')
+            self.file = ContentFile(content, name='iiif_manifest.json')
+            self.total = total
+        except FileImportError as e:
+            raise serializers.ValidationError(repr(e))
+
+    def validate_mets_uri(self, uri):
+        try:
+            content, total = clean_import_uri(uri, self.document, 'tmp.xml')
+            self.file = ContentFile(content, name='mets.xml')
+            self.total = total
+        except FileImportError as e:
+            raise serializers.ValidationError(repr(e))
+
+    def validate_upload_file(self, upload_file):
+        try:
+            parser = clean_upload_file(upload_file, self.document, self.user)
+            self.file = parser.file
+            self.total = parser.total
+        except FileImportError as e:
+            raise serializers.ValidationError(repr(e))
+
+    def validate(self, data):
+        data = super().validate(data)
+
+        # validate different modes
+        mode = data.get('mode')
+        if mode == 'iiif':
+            if 'iiif_uri' not in data:
+                raise serializers.ValidationError("'iiif_uri' is mandatory with mode 'iiif'.")
+
+        elif mode == 'mets':
+            if 'mets_type' not in data:
+                raise serializers.ValidationError("'mets_type' is mandatory with mode 'mets'.")
+            else:
+                if data.get('mets_type') == 'url':
+                    if 'mets_type' not in data:
+                        raise serializers.ValidationError("'mets_uri' is mandatory with mode 'mets'. and type 'url'")
+                    else:
+                        raise serializers.ValidationError("'upload_file' is mandatory with mode 'mets'. and type 'local'")
+
+        elif mode == 'pdf':
+            if 'upload_file' not in data:
+                raise serializers.ValidationError("'upload_file' is mandatory with mode 'pdf'.")
+
+        elif mode == 'xml':
+            if 'upload_file' not in data:
+                raise serializers.ValidationError("'upload_file' is mandatory with mode 'xml'.")
+
+        if not settings.DISABLE_QUOTAS:
+            if not self.user.has_free_cpu_minutes():
+                raise serializers.ValidationError(_("You don't have any CPU minutes left."))
+
+        return data
+
+    def create(self, validated_data):
+        imp = DocumentImport(
+            document=self.document,
+            name=validated_data.get('transcription').name if 'transcription' in validated_data else '',
+            override=validated_data.get('override') or False,
+            import_file=self.file,
+            total=self.total,
+            started_by=self.user)
+        imp.save()
+
+        document_import.delay(
+            import_pk=imp.pk,
+            user_pk=self.user.pk,
+            report_label=_('Import in %(document_name)s') % {'document_name': self.document.name}
+        )
+
+        return imp
 
 
 class DocumentTasksSerializer(serializers.ModelSerializer):

app/apps/api/urls.py
@@ -14,6 +14,7 @@ from api.views import (
     DocumentViewSet,
     GroupViewSet,
     ImageAnnotationViewSet,
+    ImportViewSet,
     LineTranscriptionViewSet,
     LineTypeViewSet,
     LineViewSet,
@@ -50,6 +51,7 @@ documents_router.register(r'parts', PartViewSet, basename='part')
 documents_router.register(r'transcriptions', DocumentTranscriptionViewSet, basename='transcription')
 documents_router.register(r'taxonomies/annotations', AnnotationTaxonomyViewSet)
 documents_router.register(r'taxonomies/components', AnnotationComponentViewSet)
+documents_router.register(r'import', ImportViewSet, basename='import')
 
 parts_router = routers.NestedSimpleRouter(documents_router, r'parts', lookup='part')
 parts_router.register(r'blocks', BlockViewSet)

app/apps/api/views.py
@@ -17,11 +17,12 @@ from rest_framework import filters, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import action
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 from rest_framework.serializers import PrimaryKeyRelatedField
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet, ReadOnlyModelViewSet
 
 from api.serializers import (
     AnnotationComponentSerializer,
@@ -38,6 +39,7 @@ from api.serializers import (
     DocumentTagSerializer,
     DocumentTasksSerializer,
     ImageAnnotationSerializer,
+    ImportSerializer,
     LineOrderSerializer,
     LineSerializer,
     LineTranscriptionSerializer,
@@ -518,6 +520,17 @@ class PartMetadataViewSet(DocumentPermissionMixin, ModelViewSet):
         context = super().get_serializer_context()
         context['part'] = DocumentPart.objects.get(pk=self.kwargs.get('part_pk'))
         return context
+
+
+class ImportViewSet(GenericViewSet, CreateModelMixin):
+    # queryset = DocumentPart.objects.all()
+    serializer_class = ImportSerializer
+
+    def create(self, request, document_pk=None):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'status': 'ok'}, status=status.HTTP_201_CREATED)
 
 
 class PartViewSet(DocumentPermissionMixin, ModelViewSet):

app/apps/imports/tests/test_api.py
@@ -467,6 +467,90 @@ class XmlImportTestCase(CoreFactoryTestCase):
         self.assertEqual(response.status_code, 400)
 
 
+class ImportEndpointTestCase(CoreFactoryTestCase):
+    def setUp(self):
+        super().setUp()
+        self.doc = self.factory.make_document()
+        self.user = self.doc.owner
+
+    def test_iiif(self):
+        self.client.force_login(self.user)
+        mock_iiif = os.path.join(os.path.dirname(__file__), 'mocks', 'iiif.json')
+
+        # Note image grabbing get mocked with the same .json file but it doesn't matter
+        with open(mock_iiif, 'rb') as fh:
+            mock_resp = mock.Mock(content=fh.read(), status_code=200)
+            with mock.patch('requests.get', return_value=mock_resp):
+                with mock.patch('imports.parsers.ParserDocument.post_process_image'):
+                    uri = reverse('api:import-list', kwargs={'document_pk': self.doc.pk})
+                    resp = self.client.post(uri, {
+                        'mode': 'iiif',
+                        'iiif_uri': 'http://some.com/url/'})
+                    self.assertEqual(resp.status_code, 201, resp.content)
+                    self.assertEqual(DocumentImport.objects.count(), 1)
+                    self.assertEqual(self.doc.parts.count(), 5)
+
+    def test_mets_local(self):
+        self.client.force_login(self.user)
+        mock_mets = os.path.join(os.path.dirname(__file__), 'samples', 'simple_archive.zip')
+        with open(mock_mets, 'rb') as fh:
+            uri = reverse('api:import-list', kwargs={'document_pk': self.doc.pk})
+            resp = self.client.post(uri, {
+                'mode': 'mets',
+                'mets_type': 'local',
+                'upload_file': SimpleUploadedFile('arch.zip', fh.read())})
+
+            self.assertEqual(resp.status_code, 201, resp.content)
+
+            self.assertEqual(DocumentImport.objects.count(), 1)
+
+            self.assertEqual(self.doc.metadatas.count(), 4)
+            self.assertEqual(self.doc.parts.count(), 4)
+
+    def test_mets_uri(self):
+        self.client.force_login(self.user)
+
+        mock_mets = os.path.join(os.path.dirname(__file__), 'samples', 'simple_archive.zip')
+
+        with open(mock_mets, 'rb') as fh:
+            mock_resp = mock.Mock(content=fh.read(), status_code=200)
+            with mock.patch('requests.get', return_value=mock_resp):
+                uri = reverse('api:import-list', kwargs={'document_pk': self.doc.pk})
+                resp = self.client.post(uri, {
+                    'mode': 'mets',
+                    'mets_type': 'uri',
+                    'mets_uri': 'http://some.com/url/'})
+
+                self.assertEqual(resp.status_code, 201, resp.content)
+
+                self.assertEqual(DocumentImport.objects.count(), 1)
+
+                self.assertEqual(self.doc.metadatas.count(), 4)
+                self.assertEqual(self.doc.parts.count(), 4)
+
+    def test_xml(self):
+        part1 = self.factory.make_part(name='part 1',
+                                       document=self.doc,
+                                       original_filename='test1.png')
+
+        self.client.force_login(self.user)
+        mock_xml = os.path.join(os.path.dirname(__file__), 'mocks', 'test.zip')
+        with open(mock_xml, 'rb') as fh:
+            uri = reverse('api:import-list', kwargs={'document_pk': self.doc.pk})
+            resp = self.client.post(uri, {
+                'mode': 'xml',
+                'upload_file': SimpleUploadedFile('test.zip', fh.read())
+            })
+            self.assertEqual(resp.status_code, 201, resp.content)
+            self.assertEqual(DocumentImport.objects.count(), 1)
+            self.assertEqual(part1.blocks.count(), 1)
+            self.assertEqual(part1.blocks.first().box, [[0, 0], [850, 0], [850, 1083], [0, 1083]])
+            self.assertEqual(part1.lines.count(), 3)
+
+    def test_pdf(self):
+        pass
+
+
 class DocumentExportTestCase(CoreFactoryTestCase):
     def setUp(self):
         super().setUp()

commit 6afd7e7697e0c2bd6c20a6d8dd43bbb3b892ed37

Fix IIIF and METS import
Fixes: 64362c843de659e5 ("Squashed commits. Adds an import endpoint")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

app/apps/imports/forms.py
@@ -112,9 +112,9 @@ class ImportForm(BootstrapFormMixin, forms.Form):
         try:
             uri = self.cleaned_data.get('uri')
             if uri:
-                parser = clean_import_uri(uri, self.document, 'tmp.json')
-                self.cleaned_data['total'] = parser.total
-                return parser.file
+                content, total = clean_import_uri(uri, self.document, 'tmp.json')
+                self.cleaned_data['total'] = total
+                return content
         except FileImportError as e:
             raise forms.ValidationError(repr(e))
 
@@ -123,10 +123,11 @@ class ImportForm(BootstrapFormMixin, forms.Form):
             uri = self.cleaned_data.get('mets_uri')
             self.mets_uri = os.path.dirname(uri)
             if uri:
-                parser = clean_import_uri(uri, self.document, 'tmp.xml',
-                                          is_mets=True, mets_base_uri=self.mets_uri)
-                self.cleaned_data['total'] = parser.total
-                return parser.file
+                content, total = clean_import_uri(uri, self.document, 'tmp.xml',
+                                                  is_mets=True,
+                                                  mets_base_uri=self.mets_uri)
+                self.cleaned_data['total'] = total
+                return content
         except FileImportError as e:
             raise forms.ValidationError(repr(e))
 