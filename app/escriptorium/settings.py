5c2ebee5956e6c4ac72067205b12cdba3a7959e0
Robin Tissot <tissotrobin@gmail.com>
2023-09-25 15:32:04 +0200

    Improvements and fixes to customizing the landing page.

--- app/escriptorium/settings.py
+++ app/escriptorium/settings.py
@@ -266,6 +266,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'homepage'),
     FRONTEND_DIR
 ]
 
