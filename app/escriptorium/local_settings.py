e947ff640d09c04857aa6c3902e492e043598deb
Stefan Weil <sw@weilnetz.de>
2023-12-22 15:10:45 +0100

    Update local settings
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/local_settings.py
+++ app/escriptorium/local_settings.py
@@ -37,7 +37,7 @@ EMAIL_HOST = 'localhost'
 
 TIME_ZONE = 'Europe/Berlin'
 USE_TZ = True
-VERSION_DATE = 'UBMA-2023-05-17'
+VERSION_DATE = 'UBMA-2024-04-26'
 
 LOCALE_PATHS = [
     os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "locale"),
@@ -68,7 +68,7 @@ INTERNAL_IPS = ['127.0.0.1']
 EMAIL_FILE_PATH = '/tmp/django-emails'
 
 KRAKEN_TRAINING_DEVICE = 'cuda:0'
-#KRAKEN_TRAINING_DEVICE = 'cpu'
+# KRAKEN_TRAINING_DEVICE = 'cpu'
 
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda r: False,  # disables it
@@ -108,7 +108,7 @@ USE_OPENSEARCH = True
 DISABLE_ELASTICSEARCH = False
 
 # Set this variable to point to your ES instance (defaults to 'localhost:9200')
-ELASTICSEARCH_URL = 'http://localhost:9200'
+ELASTICSEARCH_URL = 'http://localhost:9200/'
 
 # Set this variable to define the common ES index (defaults to 'es-transcriptions')
 # ELASTICSEARCH_COMMON_INDEX = <common_index_name>
