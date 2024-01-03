67a3dd7b3b8291a245213dcdeeb6bb1cffb0f449
Ben Silverman <ben@performantsoftware.com>
2024-01-03 15:35:12 -0500

    Allow error messages to be passed to dropdown/text fields

--- front/vue/components/TextField/TextField.vue
+++ front/vue/components/TextField/TextField.vue
@@ -9,6 +9,18 @@
                 class="escr-required"
             >*</span>
         </span>
+        <ul
+            v-if="errors && errors.length > 0"
+            class="escr-field-errors"
+        >
+            <li
+                v-for="(error, i) in errors"
+                :key="`error-${i}`"
+                class="escr-help-text escr-error-text"
+            >
+                {{ error }}
+            </li>
+        </ul>
         <input
             type="text"
             :placeholder="placeholder"
@@ -108,6 +120,13 @@ export default {
         value: {
             type: String,
             default: "",
+        },
+        /**
+         * Array of error strings.
+         */
+        errors: {
+            type: Array,
+            default: () => [],
         },
     },
 }
