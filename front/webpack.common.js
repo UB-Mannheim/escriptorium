f276d1ce9dc58a19493a62539e0c85735db18a28
Stefan Weil <sw@weilnetz.de>
2024-04-12 23:26:57 +0200

    Clean build directory
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- front/webpack.common.js
+++ front/webpack.common.js
@@ -24,6 +24,7 @@ module.exports = {
         publicPath: "",
         sourcePrefix: "/sourcePrefix/",
         workerPublicPath: "/workerPublicPath/",
+        clean: true,
     },
 
     plugins: [
