95cd8528bf666f558c185d262524321ab338d5d5
Robin Tissot <tissotrobin@gmail.com>
2024-05-28 13:22:21 +0200

    Adds a stats endpoint to both DocumentViewSet and TranscriptionViewSet with various statistics.

--- app/apps/api/views.py
+++ app/apps/api/views.py
@@ -4,7 +4,7 @@ import logging
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.db import connection, transaction
-from django.db.models import Count, Prefetch, Q
+from django.db.models import Count, F, Prefetch, Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
@@ -684,6 +684,59 @@ class DocumentViewSet(ModelViewSet):
         serializer = DocumentSerializer(document, context={'user': request.user})
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    @method_decorator(cache_page(60 * 60))  # one hour
+    @action(detail=True, methods=['get'])
+    def stats(self, request, pk=None):
+        document = self.get_object()
+        order_param = self.request.query_params.get('ordering')
+
+        if order_param in ['frequency', '-frequency']:
+            order_by = order_param
+        elif order_param in ['typology', '-typology']:
+            order_by = order_param + '__name'
+        else:
+            order_by = '-frequency'
+
+        regions = (Block.objects
+                   .filter(document_part__document=document)
+                   .values('typology_id')
+                   .annotate(typology_name=F('typology__name'),
+                             frequency=Count('*'))
+                   .order_by(order_by))
+
+        lines = (Line.objects
+                 .filter(document_part__document=document)
+                 .values('typology_id')
+                 .annotate(typology_name=F('typology__name'),
+                           frequency=Count('*'))
+                 .order_by(order_by))
+
+        if order_param in ['typology', '-typology']:
+            order_by = '-frequency'
+        elif order_param in ['taxonomy', '-taxonomy']:
+            order_by = order_param + '__name'
+
+        text_annotations = (TextAnnotation.objects
+                            .filter(part__document=document)
+                            .values('taxonomy_id')
+                            .annotate(taxonomy_name=F('taxonomy__name'),
+                                      frequency=Count('*'))
+                            .order_by(order_by))
+
+        img_annotations = (ImageAnnotation.objects
+                           .filter(part__document=document)
+                           .values('taxonomy_id')
+                           .annotate(taxonomy_name=F('taxonomy__name'),
+                                     frequency=Count('*'))
+                           .order_by(order_by))
+
+        return Response({
+            'regions': regions,
+            'lines': lines,
+            'image_annotations': img_annotations,
+            'text_annotations': text_annotations
+        })
+
 
 class TaskReportViewSet(ModelViewSet):
     queryset = TaskReport.objects.all()
@@ -869,10 +922,20 @@ class DocumentTranscriptionViewSet(DocumentPermissionMixin, ModelViewSet):
             self.get_object().archive()
             return Response(status=status.HTTP_204_NO_CONTENT)
         except ProtectedObjectException:
-            return Response("This object can not be deleted.", status=400)
+            return Response("This transcription can not be deleted.", status=400)
 
-    def characters_query(self, order_by):
+    def characters_query(self, order_param):
         transcription = self.get_object()
+
+        if order_param == 'frequency':
+            order_by = "frequency ASC"
+        elif order_param == 'char':
+            order_by = "char ASC"
+        elif order_param == '-char':
+            order_by = "char DESC"
+        else:
+            order_by = "frequency DESC"
+
         with connection.cursor() as cursor:
             cursor.execute('''
             SELECT char, count(*) as frequency
@@ -884,17 +947,22 @@ class DocumentTranscriptionViewSet(DocumentPermissionMixin, ModelViewSet):
             ''' + order_by + ';', [self.document.pk, transcription.pk])
             all_ = cursor.fetchall()
             data = [{'char': char, 'frequency': freq} for char, freq in all_]
-        return Response(data)
+
+        return data
 
     @method_decorator(cache_page(60 * 60))  # one hour
     @action(detail=True, methods=['GET'])
-    def characters(self, request, document_pk=None, pk=None):
-        return self.characters_query("frequency DESC")
+    def stats(self, request, document_pk=None, pk=None):
+        transcription = self.get_object()
+        # Note: we don't have access to OrderingFilter goodies in an @action
+        order_param = self.request.query_params.get('ordering')
+        chars = self.characters_query(order_param)
+        line_count = transcription.linetranscription_set.exclude(content='').count()
 
-    @method_decorator(cache_page(60 * 60))  # one hour
-    @action(detail=True, methods=['GET'])
-    def characters_by_char(self, request, document_pk=None, pk=None):
-        return self.characters_query("char ASC")
+        return Response({
+            'line_count': line_count,
+            'characters': chars
+        })
 
 
 class TypologyViewSet(ModelViewSet):
