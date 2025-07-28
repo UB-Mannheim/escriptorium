bd502e63451cef2b3505f9cc613a3d15b03f72d5
Stefan Weil <sw@weilnetz.de>
2025-07-28 13:08:56 +0200

    Use vue-i18n
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- front/vue/exports/documentDashboard.js
+++ front/vue/exports/documentDashboard.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import VueI18n from 'vue-i18n';
 import store from "../store";
 import Document from "../pages/Document/Document.vue";
+
+Vue.use(VueI18n);
 
 export default new Vue({
     el: "#document-dashboard",
