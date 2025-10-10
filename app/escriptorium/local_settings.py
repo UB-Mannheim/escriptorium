bcd0b43c54aece7bff9f5e15a9d523375ebb27db
Stefan Weil <sw@weilnetz.de>
2025-10-10 13:46:53 +0200

    Add contributors, credits and hosting information
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/local_settings.py
+++ app/escriptorium/local_settings.py
@@ -98,6 +98,8 @@ USE_CELERY = True
 # LOGGING['loggers']['kraken']['level'] = 'DEBUG'
 
 CUSTOM_HOME = True
+CUSTOM_CONTRIBUTORS = True
+CUSTOM_HOME = True
 
 # Uncomment the two following variables to enable customized OpenITI export modes
 EXPORT_OPENITI_MARKDOWN_ENABLED = True
