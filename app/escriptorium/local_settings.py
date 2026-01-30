94479bf2e52adc60ed87dfa86fb7e04acba7ebab
Stefan Weil <sw@weilnetz.de>
2026-01-30 16:52:37 +0100

    Update version date (now based on release v1.0.0)
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/local_settings.py
+++ app/escriptorium/local_settings.py
@@ -38,7 +38,7 @@ EMAIL_HOST = 'localhost'
 
 TIME_ZONE = 'Europe/Berlin'
 USE_TZ = True
-VERSION_DATE = 'UBMA-2026-01-28'
+VERSION_DATE = 'UBMA-2026-01-30'
 
 LANGUAGES = [
     ('de', _('German')),
