639339942620c4afe252475daacfdfcf48565421
haguili <21717377-haguili@users.noreply.gitlab.com>
2026-04-16 09:07:19 +0200

    test: update DiploLine order watcher spec for data-line-pk change

--- front/tests/unit/components/DiploLine.spec.js
+++ front/tests/unit/components/DiploLine.spec.js
@@ -6,18 +6,22 @@ describe("DiploLine", () => {
     describe("line.order watcher", () => {
         it("repositions element and updates content when parentNode exists", () => {
             const mockChildren = [{}, {}, {}];
+            const mockElement = {};
             const mockParent = {
+                removeChild: jest.fn(),
                 insertBefore: jest.fn(),
                 children: mockChildren,
             };
+            mockElement.parentNode = mockParent;
             const ctx = {
-                $el: { parentNode: mockParent },
+                getEl: jest.fn(() => mockElement),
                 line: { order: 1, currentTrans: { content: "hello" } },
                 setElContent: jest.fn(),
             };
             orderWatcher.call(ctx, 1, 0);
+            expect(mockParent.removeChild).toHaveBeenCalledWith(mockElement);
             expect(mockParent.insertBefore).toHaveBeenCalledWith(
-                ctx.$el,
+                mockElement,
                 mockChildren[1],
             );
             expect(ctx.setElContent).toHaveBeenCalledWith("hello");
