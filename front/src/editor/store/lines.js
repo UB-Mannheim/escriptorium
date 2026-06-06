a8d6978e285abcab206d49ba9f2b0425d5e31863
Stefan Weil <sw@weilnetz.de>
2026-06-06 07:44:18 +0200

    chore: fix some errors and warnings from eslint
    These errors and warnings were fixed by running `eslint --fix`:
    
        warning  Strings must use doublequote                  quotes
        error    Expected indentation of 4 spaces but found 2  indent
        error    Expected parentheses around arrow function argument                      arrow-parens
    
    There still remain lots of errors and warnings:
    
        ✖ 7517 problems (764 errors, 6753 warnings)
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- front/src/editor/store/lines.js
+++ front/src/editor/store/lines.js
@@ -243,13 +243,13 @@ export const actions = {
         } catch (err) {
             if (err.response && err.response.status === 400) {
                 console.error(
-                '[bulkUpdateLines] 400 Bad Request:',
-                {
-                    url:    err.config.url,
-                    method: err.config.method,
-                    payload: JSON.parse(err.config.data),
-                    response: err.response.data
-                }
+                    "[bulkUpdateLines] 400 Bad Request:",
+                    {
+                        url:    err.config.url,
+                        method: err.config.method,
+                        payload: JSON.parse(err.config.data),
+                        response: err.response.data
+                    }
                 )
             }
             throw err
