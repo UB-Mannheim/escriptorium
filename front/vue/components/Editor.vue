cb93042b9d0dd2f34ef678b2eedbc08b43f9d73e
Stefan Weil <sw@weilnetz.de>
2026-07-04 14:13:55 +0200

    Fix web socket URL for subpath
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- front/vue/components/Editor.vue
+++ front/vue/components/Editor.vue
@@ -73,6 +73,7 @@ import TranscriptionManagement from "./TranscriptionManagement.vue";
 import TranscriptionModal from "./TranscriptionModal.vue";
 import TranscriptionsModal from "./TranscriptionsModal/TranscriptionsModal.vue";
 import "./Editor.css";
+import { SCRIPT_NAME } from "../../src/scriptname.js";
 
 export default {
     name: "EscrEditor",
@@ -217,9 +218,7 @@ export default {
                 this.documentId
             }}`;
             const scheme = location.protocol === "https:" ? "wss:" : "ws:";
-            const msgSocket = new ReconnectingWebSocket(`${scheme}//${
-                window.location.host
-            }/ws/notif/`);
+	    const msgSocket = new ReconnectingWebSocket(`${scheme}//${window.location.host}${SCRIPT_NAME}/ws/notif/`);
             msgSocket.maxReconnectAttempts = 3;
             // intercept all websocket messages
             msgSocket.addEventListener("message", this.websocketListener);
