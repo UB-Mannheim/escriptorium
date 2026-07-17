716361adac3e6174b88bd406785e1e6c5c09b888
haguili <21717377-haguili@users.noreply.gitlab.com>
2026-07-17 10:31:44 +0200

    guard migration against unreachable celery broker

--- app/apps/core/migrations/0078_qualify_existing_ocrmodels.py
+++ app/apps/core/migrations/0078_qualify_existing_ocrmodels.py
@@ -1,4 +1,8 @@
+import logging
+
 from django.db import migrations
+
+logger = logging.getLogger(__name__)
 
 
 def qualify_existing_models(apps, schema_editor):
@@ -7,7 +11,12 @@ def qualify_existing_models(apps, schema_editor):
     OcrModel = apps.get_model('core', 'OcrModel')
     pks = OcrModel.objects.exclude(file='').exclude(file__isnull=True).values_list('pk', flat=True)
     for pk in pks:
-        qualify_model.delay(pk)
+        # don't fail the migration if the broker is unreachable
+        try:
+            qualify_model.delay(pk)
+        except Exception as e:
+            logger.warning("could not enqueue model qualification, skipping (%s)", e)
+            break
 
 
 class Migration(migrations.Migration):
