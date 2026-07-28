361c57756f41c2f4274e2da622d6862c4a79d0a8
Stefan Weil <sw@weilnetz.de>
2026-07-28 19:55:45 +0200

    Update VERSION_DATE
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- app/escriptorium/local_settings.py
+++ app/escriptorium/local_settings.py
@@ -39,7 +39,7 @@ EMAIL_HOST = 'localhost'
 
 TIME_ZONE = 'Europe/Berlin'
 USE_TZ = True
-VERSION_DATE = 'UBMA-2026-07-21'
+VERSION_DATE = 'UBMA-2026-07-28'
 
 LANGUAGES = [
     ('de', _('German')),
