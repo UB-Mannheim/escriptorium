b4e341c444fa2937bfe47dcfb07f7326e5649fe1
Stefan Weil <sw@weilnetz.de>
2026-08-10 14:27:21 +0200

    Update startup script
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- run_escriptorium.sh
+++ run_escriptorium.sh
@@ -23,14 +23,15 @@ if [ "$HOSTNAME" = "ocr-01" ]; then
     #export SPARK_LAUNCHER_OPTS=-Xmx512m
 
     cd $basedir/app
-    celery --app escriptorium worker --concurrency 16 --queues default --loglevel INFO --max-tasks-per-child=16 --optimization fair &
+    nice celery --app escriptorium worker --concurrency 24 --queues default --loglevel INFO --max-tasks-per-child=16 --optimization fair &
     # for everything that needs a java virtual machine (except search)
-    celery --app escriptorium worker --concurrency 2 --queues jvm --loglevel INFO --optimization fair &
+    nice celery --app escriptorium worker --concurrency 2 --queues jvm --loglevel INFO --optimization fair &
     # for everything that needs to be done on the spot to update the ui
     celery --app escriptorium worker --concurrency 2 --queues live --loglevel INFO --optimization fair &
-    celery --app escriptorium worker --concurrency 4 --queues low-priority --loglevel INFO --optimization fair &
+    nice celery --app escriptorium worker --concurrency 4 --queues low-priority --loglevel INFO --optimization fair &
     # for everything that could use a GPU
     celery --app escriptorium worker --concurrency 2 --queues gpu --loglevel INFO --max-tasks-per-child=1 --optimization fair &
+    nice celery --app escriptorium worker --concurrency 1 --queues intensive-inference --loglevel INFO --max-tasks-per-child=1 --optimization fair &
     python manage.py runserver --settings escriptorium.local_settings --verbosity 2 0.0.0.0:8080
     # celery worker -l INFO -E -A escriptorium --optimization fair --prefetch-multiplier 1 --queues default -c ${CELERY_MAIN_CONC:-10} --max-tasks-per-child=10
 
