540c8a102831e77386e6e5dc8b2ecba71e995813
haguili <21717377-haguili@users.noreply.gitlab.com>
2026-08-25 04:31:04 +0200

    renumber the users migration after develop

--- app/apps/users/migrations/0025_user_download_retention_days.py
+++ app/apps/users/migrations/0025_user_download_retention_days.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0023_alter_user_legacy_mode_default'),
+        ('users', '0024_user_preferred_transcription_font'),
     ]
 
     operations = [
